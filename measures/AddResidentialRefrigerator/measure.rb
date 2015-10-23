b267ec3f7a9287b0c3e1bc3dda1870921ffa42d7
Scott Horowitz <scott.horowitz@nrel.gov>
2015-10-23 17:17:25 -0600

    Update refrigerator measure. Add new schedule helper class.
    Updated refrigerator measure to latest BEopt inputs. Added
    weekday/weekend/monthly inputs for schedules and hooked up with new
    schedule helper class. Removed reference to sim.rb.

--- measures/AddResidentialRefrigerator/measure.rb
+++ measures/AddResidentialRefrigerator/measure.rb
@@ -7,8 +7,7 @@
 #see the URL below for access to C++ documentation on model objects (click on "model" in the main window to view model objects)
 # http://openstudio.nrel.gov/sites/openstudio.nrel.gov/files/nv_data/cpp_documentation_it/model/html/namespaces.html
 
-#load sim.rb
-require "#{File.dirname(__FILE__)}/resources/sim"
+require "#{File.dirname(__FILE__)}/resources/schedules"
 
 #start the measure
 class ResidentialRefrigerator < OpenStudio::Ruleset::ModelUserScript
@@ -25,26 +24,17 @@ class ResidentialRefrigerator < OpenStudio::Ruleset::ModelUserScript
     
 	#TODO: New argument for demand response for fridges (alternate schedules if automatic DR control is specified)
 	
-	#make a choice argument for whether Benchmark fraction or annual energy consumption is specified
-	chs = OpenStudio::StringVector.new
-	chs << "Benchmark" 
-	chs << "Annual Energy Consumption"
-	
-	selected_fridge = OpenStudio::Ruleset::OSArgument::makeChoiceArgument("selected_fridge", chs, true)
-	selected_fridge.setDisplayName("Refrigerator Energy Consumption Option")
-	args << selected_fridge
-	
 	#make a double argument for user defined fridge options
 	fridge_E = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("fridge_E",true)
-	fridge_E.setDisplayName("User Defined Refrigerator Annual Energy Consumption (kWh/yr)")
-	fridge_E.setDefaultValue(0)
+	fridge_E.setDisplayName("Rated Annual Consumption (kWh/yr)")
+	fridge_E.setDefaultValue(434)
 	args << fridge_E
 	
-	#make a double argument for BA Benchamrk multiplier
-	bab_mult = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("BAB_mult")
-	bab_mult.setDisplayName("Building America Benchmark Multiplier")
-	bab_mult.setDefaultValue(1)
-	args << bab_mult
+	#make a double argument for Occupancy Energy Multiplier
+	mult = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("mult")
+	mult.setDisplayName("Occupancy Energy Multiplier")
+	mult.setDefaultValue(1)
+	args << mult
 	
 	#make a choice argument for which zone to put the space in
 	#make a choice argument for model objects
@@ -72,7 +62,25 @@ class ResidentialRefrigerator < OpenStudio::Ruleset::ModelUserScript
     space_type.setDisplayName("Select the space where the refrigerator is located")
     space_type.setDefaultValue("*None*") #if none is chosen this will error out
     args << space_type
+	
+	#Make a string argument for 24 weekday schedule values
+	weekday_sch = OpenStudio::Ruleset::OSArgument::makeStringArgument("weekday_sch")
+	weekday_sch.setDisplayName("Specify the 24-hour weekday schedule")
+	weekday_sch.setDefaultValue("0.040, 0.039, 0.038, 0.037, 0.036, 0.036, 0.038, 0.040, 0.041, 0.041, 0.040, 0.040, 0.042, 0.042, 0.042, 0.041, 0.044, 0.048, 0.050, 0.048, 0.047, 0.046, 0.044, 0.041")
+	args << weekday_sch
     
+	#Make a string argument for 24 weekend schedule values
+	weekend_sch = OpenStudio::Ruleset::OSArgument::makeStringArgument("weekend_sch")
+	weekend_sch.setDisplayName("Specify the 24-hour weekend schedule")
+	weekend_sch.setDefaultValue("0.040, 0.039, 0.038, 0.037, 0.036, 0.036, 0.038, 0.040, 0.041, 0.041, 0.040, 0.040, 0.042, 0.042, 0.042, 0.041, 0.044, 0.048, 0.050, 0.048, 0.047, 0.046, 0.044, 0.041")
+	args << weekend_sch
+
+	#Make a string argument for 12 monthly schedule values
+	monthly_sch = OpenStudio::Ruleset::OSArgument::makeStringArgument("monthly_sch")
+	monthly_sch.setDisplayName("Specify the 12-month schedule")
+	monthly_sch.setDefaultValue("0.837, 0.835, 1.084, 1.084, 1.084, 1.096, 1.096, 1.096, 1.096, 0.931, 0.925, 0.837")
+	args << monthly_sch
+
     return args
   end #end the arguments method
 
@@ -84,172 +92,84 @@ class ResidentialRefrigerator < OpenStudio::Ruleset::ModelUserScript
     if not runner.validateUserArguments(arguments(model), user_arguments)
       return false
     end
-
+	
     #assign the user inputs to variables
-    selected_fridge = runner.getStringArgumentValue("selected_fridge",user_arguments)
 	space_type_r = runner.getStringArgumentValue("space_type",user_arguments)
+	mult = runner.getDoubleArgumentValue("mult",user_arguments)
     fridge_E = runner.getDoubleArgumentValue("fridge_E",user_arguments)
-	bab_mult = runner.getDoubleArgumentValue("BAB_mult",user_arguments)
-	
-	#warning if fridge E is selected and BAB multiplier is specified or vice versa
-	if selected_fridge == "Annual Energy Consumption" and bab_mult != 1
-		runner.registerWarning("Annual energy consumption is selected with a user specified benchmark multiplier. The multiplier will not be used.")
-	end
-	
-	if selected_fridge == "Benchmark" and fridge_E != 0
-		runner.registerWarning("Benchmark is selected with a user specified annual energy consumption. The annual energy consumption will not be used.")
-	end
+	weekday_sch = runner.getStringArgumentValue("weekday_sch",user_arguments)
+	weekend_sch = runner.getStringArgumentValue("weekend_sch",user_arguments)
+	monthly_sch = runner.getStringArgumentValue("monthly_sch",user_arguments)
 	
 	#if fridge energy consumption is defined, check for reasonable energy consumption
-	if selected_fridge == "Annual Energy Consumption" 
-		if fridge_E < 0
-			runner.registerError("Refrigerator energy consumption must be greater than 0")
-		elsif fridge_E < 100
-			runner.registerError("Refrigerator energy consumption seems low, double check inputs") 
-		elsif fridge_E > 3000
-			runner.registerError("Refrigerator energy consumption seems high, double check inputs") 
-		end
-	end
-	
-	#if BAB multiplier is defined, make sure it is positive and nonzero
-	if selected_fridge == "Benchmark" and bab_mult <= 0
-		runner.registerError("Benchmark multiplier must be positive and greater than zero, double check inputs")
+	if fridge_E < 0
+		runner.registerError("Refrigerator energy consumption must be greater than or equal to 0")
+		return false
+	elsif fridge_E < 100
+		runner.registerError("Refrigerator energy consumption seems low, double check inputs") 
+		return false
+	elsif fridge_E > 3000
+		runner.registerError("Refrigerator energy consumption seems high, double check inputs") 
+		return false
 	end
 	
 	#Calculate fridge daily energy use
-	if selected_fridge == "Annual Energy Consumption"
-		fridge_daily = fridge_E/365.0
-		fridge_ann = fridge_E
-	else
-		fridge_daily = (434 * bab_mult)/365.0
-		fridge_ann = 434 * bab_mult
-	end
+	fridge_ann = fridge_E*mult
+	fridge_daily = fridge_ann/365.0
 
-	refrigeratorval = Process_refrigerator.new
+    #hard coded convective, radiative, latent, and lost fractions for fridges
+    fridge_lat = 0
+    fridge_rad = 0
+    fridge_lost = 0
+    fridge_conv = 1
 	
-	#pull schedule values and gain fractions from sim
-	fridge_lat = refrigeratorval.Fridge_lat
-	fridge_conv = refrigeratorval.Fridge_conv
-	fridge_rad = refrigeratorval.Fridge_rad
-	fridge_lost = refrigeratorval.Fridge_lost
-	
-	monthly_mult = refrigeratorval.Monthly_mult_fridge
-	weekday_hourly = refrigeratorval.Weekday_hourly_fridge
-	weekend_hourly = refrigeratorval.Weekend_hourly_fridge
-	maxval = refrigeratorval.Maxval_fridge
-	sum_wkdy = refrigeratorval.Sum_wkdy
-	sch_adjust = 1/sum_wkdy
-	
-	#get fridge max power
-	fridge_max = (fridge_daily * maxval * 1000 * sch_adjust)
+	s = Schedule.new(weekday_sch, weekend_sch, monthly_sch, runner)
+	if not s.validated?
+		return false
+	end
+	fridge_max = s.calcDesignLevel(fridge_daily)
 	
 	#add refrigerator to the selected space
+	fridge_name = "residential_refrigerator"
 	has_fridge = 0
 	replace_fridge = 0
 	num_equip = 1
 	model.getSpaceTypes.each do |spaceType|
 		spacename = spaceType.name.to_s
 		spacehandle = spaceType.handle.to_s
-		#runner.registerWarning("#{spaceType}")
 		if spacehandle == space_type_r #add refrigerator
 			space_equipments = spaceType.electricEquipment
 			space_equipments.each do |space_equipment|
-				if space_equipment.electricEquipmentDefinition.name.get.to_s == "residential_refrigerator" #TODO: double check that this actually gets equipment name
+				if space_equipment.electricEquipmentDefinition.name.get.to_s == fridge_name #TODO: double check that this actually gets equipment name
 					has_fridge = 1
 					runner.registerWarning("This space already has a refrigerator, the existing refrigerator will be replaced with the the currently selected option")
 					space_equipment.electricEquipmentDefinition.setDesignLevel(fridge_max)
+					
+					#Assign schedule
+					#TODO: This apparently leaves the old, unused schedule in the file
+					refrig_ruleset = s.createSchedule(model, fridge_name)
+					space_equipment.setSchedule(refrig_ruleset)
+					
 					num_equip += 1
 					replace_fridge = 1
 				end
 			end
 			if has_fridge == 0 
-
-				#add refrigerator schedule
 				has_fridge = 1
-				#refrig_sch = OpenStudio::Model::ScheduleRuleset.new(model)
-				#refrig_sch.setName("refrig_schedule")
-				refrig_wkdy = ['0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' ]
-				refrig_wknd = ['0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' ]
-				refrig_wk = ['0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' ]
-				time = ['0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0']
-				wkdy_refrig_rule = ['0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' ]
-				wknd_refrig_rule = ['0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' ]
-				day_endm = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
-				day_startm = [0, 1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
 				
-				refrig_ruleset = OpenStudio::Model::ScheduleRuleset.new(model)
-				refrig_ruleset.setName("refrigerator_ruleset")
-				
-				
-				for m in 1..12
-					date_s = OpenStudio::Date::fromDayOfYear(day_startm[m])
-					date_e = OpenStudio::Date::fromDayOfYear(day_endm[m])
-					for w in 1..2
-						if w == 1
-							wkdy_refrig_rule[m] = OpenStudio::Model::ScheduleRule.new(refrig_ruleset)
-							wkdy_refrig_rule[m].setName("fridge_weekday_ruleset#{m}")
-							wkdy_refrig_rule
-							refrig_wkdy[m] = wkdy_refrig_rule[m].daySchedule
-							refrig_wkdy[m].setName("RefrigeratorWeekday#{m}")
-							for h in 1..24
-								time[h] = OpenStudio::Time.new(0,h,0,0)
-								val = (monthly_mult[m-1].to_f*weekday_hourly[h-1].to_f)/maxval
-								#runner.registerWarning("#{monthly_mult[m-1]}")
-								#runner.registerWarning("#{weekday_hourly[h]}")
-								#runner.registerWarning("#{val}")
-								refrig_wkdy[m].addValue(time[h],val)
-							end
-							wkdy_refrig_rule[m].setApplySunday(false)
-							wkdy_refrig_rule[m].setApplyMonday(true)
-							wkdy_refrig_rule[m].setApplyTuesday(true)
-							wkdy_refrig_rule[m].setApplyWednesday(true)
-							wkdy_refrig_rule[m].setApplyThursday(true)
-							wkdy_refrig_rule[m].setApplyFriday(true)
-							wkdy_refrig_rule[m].setApplySaturday(false)
-							wkdy_refrig_rule[m].setStartDate(date_s)
-							wkdy_refrig_rule[m].setEndDate(date_e)
-							
-						elsif w == 2
-							wknd_refrig_rule[m] = OpenStudio::Model::ScheduleRule.new(refrig_ruleset)
-							wknd_refrig_rule[m].setName("fridge_weekday_ruleset#{m}")
-							refrig_wknd[m] = wknd_refrig_rule[m].daySchedule
-							refrig_wknd[m].setName("RefrigeratorWeekend#{m}")
-							for h in 1..24
-								time[h] = OpenStudio::Time.new(0,h,0,0)
-								val = (monthly_mult[m-1].to_f*weekend_hourly[h-1].to_f)/maxval
-								refrig_wknd[m].addValue(time[h],val)
-							end
-							wknd_refrig_rule[m].setApplySunday(true)
-							wknd_refrig_rule[m].setApplyMonday(false)
-							wknd_refrig_rule[m].setApplyTuesday(false)
-							wknd_refrig_rule[m].setApplyWednesday(false)
-							wknd_refrig_rule[m].setApplyThursday(false)
-							wknd_refrig_rule[m].setApplyFriday(false)
-							wknd_refrig_rule[m].setApplySaturday(true)
-							wknd_refrig_rule[m].setStartDate(date_s)
-							wknd_refrig_rule[m].setEndDate(date_e)
-						end
-					end
-				end
-				
-				sumDesSch = refrig_wkdy[6]
-				sumDesSch.setName("RefrigeratorSummer")
-				winDesSch = refrig_wkdy[1]
-				winDesSch.setName("RefrigeratorWinter")
-				refrig_ruleset.setSummerDesignDaySchedule(sumDesSch)
-				refrig_ruleset.setWinterDesignDaySchedule(winDesSch)
-					
 				#Add electric equipment for the fridge
 				frg_def = OpenStudio::Model::ElectricEquipmentDefinition.new(model)
 				frg = OpenStudio::Model::ElectricEquipment.new(frg_def)
-				frg.setName("residential_refrigerator")
+				frg.setName(fridge_name)
 				frg.setSpaceType(spaceType)
-				frg_def.setName("residential_refrigerator")
+				frg_def.setName(fridge_name)
 				frg_def.setDesignLevel(fridge_max)
 				frg_def.setFractionRadiant(fridge_rad)
 				frg_def.setFractionLatent(fridge_lat)
 				frg_def.setFractionLost(fridge_lost)
 				
+				#Assign schedule
+				refrig_ruleset = s.createSchedule(model, fridge_name)
 				frg.setSchedule(refrig_ruleset)
 				
 			end
---
+++ resources/schedules.rb
@@ -0,0 +1,137 @@
+class Schedule
+    def initialize(weekday_hourly_values, weekend_hourly_values, monthly_values, runner)
+		@validated = true
+        @weekday_hourly_values = validateValues(weekday_hourly_values, 24, runner, "weekday")
+	    @weekend_hourly_values = validateValues(weekend_hourly_values, 24, runner, "weekend")
+	    @monthly_values = validateValues(monthly_values, 12, runner, "monthly")
+		@maxval = maxval()
+		@schadjust = schadjust()
+    end
+  
+	def validateValues(values_str, num_values, runner, sch_name)
+	    begin
+			vals = values_str.split(",")
+			vals.each do |val|
+				if not valid_float?(val)
+					runner.registerError(num_values.to_s + " comma-separated numbers must be entered for the " + sch_name + " schedule.")
+					@validated = false
+				end
+			end
+	        floats = vals.map {|i| i.to_f}
+			if floats.length != num_values
+			    runner.registerError(num_values.to_s + " comma-separated numbers must be entered for the " + sch_name + " schedule.")
+				@validated = false
+			end
+	    rescue
+	        runner.registerError(num_values.to_s + " comma-separated numbers must be entered for the " + sch_name + " schedule.")
+			@validated = false
+		end
+		return floats
+	end
+	
+	def validated?
+		return @validated
+	end
+
+	def valid_float?(str)
+		!!Float(str) rescue false
+	end
+  
+	def maxval()
+	    if @weekday_hourly_values.max > @weekend_hourly_values.max
+		  return @monthly_values.max * @weekday_hourly_values.max
+		else
+		  return @monthly_values.max * @weekend_hourly_values.max
+		end
+	end
+	
+	def schadjust()
+		#if sum != 1, normalize to get correct max val
+		sum_wkdy = 0
+		sum_wknd = 0
+		@weekday_hourly_values.each do |v|
+		    sum_wkdy = sum_wkdy + v
+		end
+		@weekend_hourly_values.each do |v|
+		    sum_wknd = sum_wknd + v
+		end
+		if sum_wkdy < sum_wknd
+			return 1/sum_wknd
+		end
+		return 1/sum_wkdy
+	end
+	
+	def calcDesignLevel(energy_kwh)
+		return energy_kwh * @maxval * 1000 * @schadjust
+	end
+  
+    def createSchedule(model, sch_name)
+		wkdy = []
+		wknd = []
+		day_endm = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+		day_startm = [0, 1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
+		
+		time = ['0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0' '0']
+		for h in 1..24
+			time[h] = OpenStudio::Time.new(0,h,0,0)
+		end
+
+		ruleset = OpenStudio::Model::ScheduleRuleset.new(model)
+		ruleset.setName(sch_name + "_annual_schedule")
+		
+		for m in 1..12
+			date_s = OpenStudio::Date::fromDayOfYear(day_startm[m])
+			date_e = OpenStudio::Date::fromDayOfYear(day_endm[m])
+			for w in 1..2
+				if w == 1
+					wkdy_rule = OpenStudio::Model::ScheduleRule.new(ruleset)
+					wkdy_rule.setName(sch_name + "_weekday_ruleset#{m}")
+					wkdy[m] = wkdy_rule.daySchedule
+					wkdy[m].setName(sch_name + "_weekday#{m}")
+					for h in 1..24
+						val = (@monthly_values[m-1].to_f*@weekday_hourly_values[h-1].to_f)/@maxval
+						wkdy[m].addValue(time[h],val)
+					end
+					wkdy_rule.setApplySunday(false)
+					wkdy_rule.setApplyMonday(true)
+					wkdy_rule.setApplyTuesday(true)
+					wkdy_rule.setApplyWednesday(true)
+					wkdy_rule.setApplyThursday(true)
+					wkdy_rule.setApplyFriday(true)
+					wkdy_rule.setApplySaturday(false)
+					wkdy_rule.setStartDate(date_s)
+					wkdy_rule.setEndDate(date_e)
+					
+				elsif w == 2
+					wknd_rule = OpenStudio::Model::ScheduleRule.new(ruleset)
+					wknd_rule.setName(sch_name + "_weekend_ruleset#{m}")
+					wknd[m] = wknd_rule.daySchedule
+					wknd[m].setName(sch_name + "_weekend#{m}")
+					for h in 1..24
+						val = (@monthly_values[m-1].to_f*@weekend_hourly_values[h-1].to_f)/@maxval
+						wknd[m].addValue(time[h],val)
+					end
+					wknd_rule.setApplySunday(true)
+					wknd_rule.setApplyMonday(false)
+					wknd_rule.setApplyTuesday(false)
+					wknd_rule.setApplyWednesday(false)
+					wknd_rule.setApplyThursday(false)
+					wknd_rule.setApplyFriday(false)
+					wknd_rule.setApplySaturday(true)
+					wknd_rule.setStartDate(date_s)
+					wknd_rule.setEndDate(date_e)
+				end
+			end
+		end
+		
+		sumDesSch = wkdy[6] # FIXME: Where did this come from?
+		sumDesSch.setName(sch_name + "_summer")
+		winDesSch = wkdy[1] # FIXME: Where did this come from?
+		winDesSch.setName(sch_name + "_winter")
+		ruleset.setSummerDesignDaySchedule(sumDesSch)
+		ruleset.setWinterDesignDaySchedule(winDesSch)
+		
+		return ruleset
+    end
+	
+end
