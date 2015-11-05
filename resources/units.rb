67db76116ead1b8e513b89c248646f282d9b973e
Scott Horowitz <scott.horowitz@nrel.gov>
2015-11-05 15:23:38 -0700

    Switch to OpenStudio.convert in schedules.rb

--- resources/units.rb
+++ resources/units.rb
@@ -1,13 +1,14 @@
 class Units
 
-	def self.therms2kWh(x)
-		# therms -> kWh
-		return x*29.3
-	end
+	# Contains unit conversions not available in OpenStudio.convert.
 	
-	def self.kWh2therms(x)
-		# kWh -> therms
-		return x/29.3
+	# See http://nrel.github.io/OpenStudio-user-documentation/reference/measure_code_examples/
+	# for available OS unit conversions. Note that this list may not be complete, so try out
+	# new unit conversions before adding them here.
+
+	def self.knots2m_s(knots)
+		# knots -> m/s
+		return 0.51444444*knots
 	end
   
 end
