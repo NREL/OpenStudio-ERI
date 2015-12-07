9f82fb233f2e5ee0e7c60b859f26d645bf8e4951
Scott Horowitz <scott.horowitz@nrel.gov>
2015-12-07 12:40:09 -0700

    Split Cooking Range into Elec and Gas measures
    Simplifies the measures and allows them to be characterized as Electric
    Equipment or Gas Equipment.

--- measures/ResidentialCookingRangeGas/measure.rb
+++ measures/ResidentialCookingRangeGas/measure.rb
@@ -5,7 +5,7 @@ require "#{File.dirname(__FILE__)}/resources/constants"
 class ResidentialCookingRange < OpenStudio::Ruleset::ModelUserScript
   
   def name
-    return "Add/Replace Residential Electric Cooking Range"
+    return "Add/Replace Residential Gas Cooking Range"
   end
   
   def description
@@ -13,7 +13,7 @@ class ResidentialCookingRange < OpenStudio::Ruleset::ModelUserScript
   end
   
   def modeler_description
-    return "Since there is no Cooking Range object in OpenStudio/EnergyPlus, we look for an ElectricEquipment object with the name that denotes it is a residential cooking range. If one is found, it is replaced with the specified properties. Otherwise, a new such object is added to the model."
+    return "Since there is no Cooking Range object in OpenStudio/EnergyPlus, we look for a GasEquipment object with the name that denotes it is a residential cooking range. If one is found, it is replaced with the specified properties. Otherwise, a new such object is added to the model."
   end
   
   #define the arguments that the user will input
@@ -48,6 +48,13 @@ class ResidentialCookingRange < OpenStudio::Ruleset::ModelUserScript
 	o_ef.setDefaultValue(0.11)
 	args << o_ef
 	
+	#make a boolean argument for has electric ignition
+	e_ignition = OpenStudio::Ruleset::OSArgument::makeBoolArgument("e_ignition")
+	e_ignition.setDisplayName("Has Electronic Ignition")
+	e_ignition.setDescription("For gas/propane cooking ranges with electronic ignition, an extra (40 + 13.3x(#BR)) kWh/yr of electricity will be included.")
+	e_ignition.setDefaultValue(false)
+	args << e_ignition
+
 	#make a double argument for Occupancy Energy Multiplier
 	mult = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("mult")
 	mult.setDisplayName("Occupancy Energy Multiplier")
@@ -120,6 +127,7 @@ class ResidentialCookingRange < OpenStudio::Ruleset::ModelUserScript
 	num_br = runner.getStringArgumentValue("Num_Br", user_arguments)
 	c_ef = runner.getDoubleArgumentValue("C_ef",user_arguments)
 	o_ef = runner.getDoubleArgumentValue("O_ef",user_arguments)
+	e_ignition = runner.getBoolArgumentValue("e_ignition",user_arguments)
 	mult = runner.getDoubleArgumentValue("mult",user_arguments)
 	space_type_r = runner.getStringArgumentValue("space_type",user_arguments)
 	weekday_sch = runner.getStringArgumentValue("weekday_sch",user_arguments)
@@ -138,14 +146,23 @@ class ResidentialCookingRange < OpenStudio::Ruleset::ModelUserScript
 		return false
 	end
 	
-	#Calculate electric range daily energy use
-    range_ann_e = ((86.5 + 28.9 * num_br) / c_ef + (14.6 + 4.9 * num_br) / o_ef)*mult #kWh/yr
+	#Calculate gas range daily energy use
+    range_ann_g = ((2.64 + 0.88 * num_br) / c_ef + (0.44 + 0.15 * num_br) / o_ef)*mult # therm/yr
+    if e_ignition == true
+        range_ann_i = (40 + 13.3 * num_br)*mult #kWh/yr
+    else
+        range_ann_i = 0
+    end
 	
     #hard coded convective, radiative, latent, and lost fractions
 	range_lat_e = 0.3
 	range_conv_e = 0.16
 	range_rad_e = 0.24
 	range_lost_e = 1 - range_lat_e - range_conv_e - range_rad_e
+	range_lat_g = 0.2
+	range_conv_g = 0.12
+	range_rad_g = 0.18
+	range_lost_g = 1 - range_lat_g - range_conv_g - range_rad_g
 
 	obj_name = Constants.ObjectNameCookingRange
 	obj_name_e = obj_name + "_" + Constants.FuelTypeElectric
@@ -155,12 +172,13 @@ class ResidentialCookingRange < OpenStudio::Ruleset::ModelUserScript
 	if not sch.validated?
 		return false
 	end
-    design_level_e = sch.calcDesignLevelElec(range_ann_e/365.0)
+    design_level_g = sch.calcDesignLevelGas(range_ann_g/365.0)
+    design_level_i = sch.calcDesignLevelElec(range_ann_i/365.0)
 
 	#add range to the selected space
-	has_elec_range = 0
-	replace_elec_range = 0
-	remove_g_range = 0
+	has_gas_range = 0
+	replace_gas_range = 0
+	remove_e_range = 0
 	model.getSpaceTypes.each do |spaceType|
 		spacename = spaceType.name.to_s
 		spacehandle = spaceType.handle.to_s
@@ -168,51 +186,80 @@ class ResidentialCookingRange < OpenStudio::Ruleset::ModelUserScript
 			space_equipments_g = spaceType.gasEquipment
 			space_equipments_g.each do |space_equipment_g| #check for an existing gas range
 				if space_equipment_g.gasEquipmentDefinition.name.get.to_s == obj_name_g
-                    runner.registerWarning("This space already has a gas range. The existing gas range will be removed and replaced with the specified electric range.")
-                    space_equipment_g.remove
-                    remove_g_range = 1
+                    has_gas_range = 1
+                    runner.registerWarning("This space already has a gas range. The existing gas range will be replaced with the specified gas range.")
+                    space_equipment_g.gasEquipmentDefinition.setDesignLevel(design_level_g)
+                    sch.setSchedule(space_equipment_g)
+                    replace_gas_range = 1
 				end
 			end
 			space_equipments_e = spaceType.electricEquipment
 			space_equipments_e.each do |space_equipment_e|
 				if space_equipment_e.electricEquipmentDefinition.name.get.to_s == obj_name_e
-                    has_elec_range = 1
                     runner.registerWarning("This space already has an electric range. The existing range will be replaced with the the currently selected option.")
-                    space_equipment_e.electricEquipmentDefinition.setDesignLevel(design_level_e)
-                    sch.setSchedule(space_equipment_e)
-                    replace_elec_range = 1
-				elsif space_equipment_e.electricEquipmentDefinition.name.get.to_s == obj_name_i
                     space_equipment_e.remove
+                    remove_e_range = 1
+				elsif space_equipment_e.electricEquipmentDefinition.name.get.to_s == obj_name_i
+					if e_ignition == true
+						space_equipment_e.electricEquipmentDefinition.setDesignLevel(design_level_i)
+						sch.setSchedule(space_equipment_e)
+					else
+						space_equipment_e.remove
+					end
 				end
 			end
 			
-			if has_elec_range == 0
+			if has_gas_range == 0
 
-                has_elec_range = 1
+                has_gas_range = 1
 				
 				#Add equipment for the range
-                rng_def = OpenStudio::Model::ElectricEquipmentDefinition.new(model)
-                rng = OpenStudio::Model::ElectricEquipment.new(rng_def)
-                rng.setName(obj_name_e)
+                rng_def = OpenStudio::Model::GasEquipmentDefinition.new(model)
+                rng = OpenStudio::Model::GasEquipment.new(rng_def)
+                rng.setName(obj_name_g)
                 rng.setSpaceType(spaceType)
-                rng_def.setName(obj_name_e)
-                rng_def.setDesignLevel(design_level_e)
-                rng_def.setFractionRadiant(range_rad_e)
-                rng_def.setFractionLatent(range_lat_e)
-                rng_def.setFractionLost(range_lost_e)
+                rng_def.setName(obj_name_g)
+                rng_def.setDesignLevel(design_level_g)
+                rng_def.setFractionRadiant(range_rad_g)
+                rng_def.setFractionLatent(range_lat_g)
+                rng_def.setFractionLost(range_lost_g)
                 sch.setSchedule(rng)
+                if e_ignition == true
+                    rng_def2 = OpenStudio::Model::ElectricEquipmentDefinition.new(model)
+                    rng2 = OpenStudio::Model::ElectricEquipment.new(rng_def2)
+                    rng2.setName(obj_name_i)
+                    rng2.setSpaceType(spaceType)
+                    rng_def2.setName(obj_name_i)
+                    rng_def2.setDesignLevel(design_level_i)
+                    rng_def2.setFractionRadiant(range_rad_e)
+                    rng_def2.setFractionLatent(range_lat_e)
+                    rng_def2.setFractionLost(range_lost_e)
+                    sch.setSchedule(rng2)
+                end
 			end
 		end
 	end
 
     #reporting final condition of model
-	if has_elec_range == 1
-		if replace_elec_range == 1
-			runner.registerFinalCondition("The existing electric range has been replaced by one with #{range_ann_e.round} kWhs annual energy consumption.")
-		elsif remove_g_range == 1
-			runner.registerFinalCondition("The existing gas range has been replaced by an electric range with #{range_ann_e.round} kWhs annual energy consumption.")
+	if has_gas_range == 1
+		if replace_gas_range == 1
+			if e_ignition == true
+				runner.registerFinalCondition("The existing gas range has been replaced by one with #{range_ann_g.round} therms and #{range_ann_i.round} kWhs annual energy consumption.")
+			else
+				runner.registerFinalCondition("The existing gas range has been replaced by one with #{range_ann_g.round} therms annual energy consumption.")
+			end
+		elsif remove_e_range == 1
+			if e_ignition == true
+				runner.registerFinalCondition("The existing electric range has been replaced by a gas range with #{range_ann_g.round} therms and #{range_ann_i.round} kWhs annual energy consumption.")
+			else
+				runner.registerFinalCondition("The existing electric range has been replaced by a gas range with #{range_ann_g.round} therms annual energy consumption.")
+			end
 		else
-			runner.registerFinalCondition("An electric range has been added with #{range_ann_e.round} kWhs annual energy consumption.")
+			if e_ignition == true
+				runner.registerFinalCondition("A gas range has been added with #{range_ann_g.round} therms and #{range_ann_i.round} kWhs annual energy consumption.")
+			else
+				runner.registerFinalCondition("A gas range has been added with #{range_ann_g.round} therms annual energy consumption.")
+			end
 		end
 	else
 		runner.registerFinalCondition("Range was not added to #{space_type_r}.")
