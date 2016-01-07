70d281b8f03416c6f5502c241f4a20113982a95c
Scott Horowitz <scott.horowitz@nrel.gov>
2016-01-06 17:04:01 -0700

    Fixed missing resource requirements for 2 measures.

--- measures/CreateResidentialDoorArea/measure.rb
+++ measures/CreateResidentialDoorArea/measure.rb
@@ -1,6 +1,8 @@
 # see the URL below for information on how to write OpenStudio measures
 # http://nrel.github.io/OpenStudio-user-documentation/measures/measure_writing_guide/
 
+require "#{File.dirname(__FILE__)}/resources/constants"
+
 # start the measure
 class CreateResidentialDoorArea < OpenStudio::Ruleset::ModelUserScript
 
