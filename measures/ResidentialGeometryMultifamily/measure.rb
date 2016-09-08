faf370cc05e2fff9c7f242bf2f2cfcb39542314b
joseph-robertson <Eric.Wilson@nrel.gov>
2016-09-08 08:58:37 -0600

    Removing townhouse and apartment geometry measures.

--- measures/ResidentialGeometryMultifamily/measure.rb
+++ measures/ResidentialGeometryMultifamily/measure.rb
@@ -130,6 +130,13 @@ class CreateResidentialMultifamilyGeometry < OpenStudio::Ruleset::ModelUserScrip
     inset_pos.setDefaultValue("Right")
     args << inset_pos
     
+    #make an argument for using zone multipliers
+    # use_zone_mult = OpenStudio::Ruleset::OSArgument::makeBoolArgument("use_zone_mult", true)
+    # use_zone_mult.setDisplayName("Use Zone Multipliers?")
+    # use_zone_mult.setDescription("Model only one interior unit with its thermal zone multiplier equal to the number of interior units.")
+    # use_zone_mult.setDefaultValue(false)
+    # args << use_zone_mult    
+    
     return args
   end
 
@@ -154,6 +161,7 @@ class CreateResidentialMultifamilyGeometry < OpenStudio::Ruleset::ModelUserScrip
     inset_width = OpenStudio::convert(runner.getDoubleArgumentValue("inset_width",user_arguments),"ft","m").get
     inset_depth = OpenStudio::convert(runner.getDoubleArgumentValue("inset_depth",user_arguments),"ft","m").get
     inset_pos = runner.getStringArgumentValue("inset_pos",user_arguments)
+    # use_zone_mult = runner.getBoolArgumentValue("use_zone_mult",user_arguments)
     
     # error checking
     if model.getSpaces.size > 0
@@ -575,6 +583,58 @@ class CreateResidentialMultifamilyGeometry < OpenStudio::Ruleset::ModelUserScrip
     
     # Store dwelling unit information (for consistency with multifamily buildings)
     model.getBuilding.setStandardsNumberOfLivingUnits(num_units)
+    
+    # (1...num_units).to_a.each do |unit_num|
+
+      # if use_zone_mult and (unit_num == 1 or unit_num + 1 == num_units)
+        # living_zone = OpenStudio::Model::ThermalZone.new(model)
+        # living_zone.setName(Constants.LivingZone)
+        # if unit_num == 1
+          # living_zone.setMultiplier(num_units - 2)
+        # end
+      # elsif !use_zone_mult
+        # living_zone = OpenStudio::Model::ThermalZone.new(model)
+        # living_zone.setName(Constants.LivingZone)
+      # end
+      
+      # new_living_spaces = []
+      # living_spaces.each do |living_space|
+    
+        # new_living_space = living_space.clone.to_Space.get
+      
+        # m = OpenStudio::Matrix.new(4,4,0)
+        # m[0,0] = 1
+        # m[1,1] = 1
+        # m[2,2] = 1
+        # m[3,3] = 1
+        # if (unit_num + 1) % 2 == 0
+          # m[1,3] = -offset
+        # end
+        # m[0,3] = -unit_num * x
+        # new_living_space.changeTransformation(OpenStudio::Transformation.new(m))
+        # new_living_space.setXOrigin(0)
+        # new_living_space.setYOrigin(0)
+        # if (use_zone_mult and (unit_num == 1 or unit_num + 1 == num_units)) or !use_zone_mult
+          # new_living_space.setThermalZone(living_zone)
+        # else
+          # new_living_space.resetThermalZone          
+        # end        
+     
+        # new_living_spaces << new_living_space
+      
+      # end
+      
+      # if unit_num == 1 or !use_zone_mult
+        # Geometry.set_unit_beds_baths_spaces(model, unit_num + 1, new_living_spaces)
+      # elsif use_zone_mult and unit_num + 1 == num_units
+        # Geometry.set_unit_beds_baths_spaces(model, 3, new_living_spaces)
+      # end
+      
+    # end
+    
+    # if use_zone_mult
+      # num_units = 3
+    # end    
     
     return true
 
