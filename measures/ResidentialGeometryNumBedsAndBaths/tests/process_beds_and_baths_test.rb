989545b0dc67dbe0910b09b4dd40ef1f42b90b21
Joe Robertson <joseph.robertson@nrel.gov>
2016-10-04 12:34:09 -0600

    Updating unit tests for beds and baths measure.

--- measures/ResidentialGeometryNumBedsAndBaths/tests/process_beds_and_baths_test.rb
+++ measures/ResidentialGeometryNumBedsAndBaths/tests/process_beds_and_baths_test.rb
@@ -11,49 +11,49 @@ class BedroomsAndBathroomsTest < MiniTest::Test
     args_hash = {}
     args_hash["Num_Br"] = "3.0, 3.0, 3.0"
     args_hash["Num_Ba"] = "2.0, 2.0"
-    result = _test_error("2000sqft_2story_FB_GRG_UA.osm", args_hash)
+    result = _test_error("singlefamily_detached.osm", args_hash)
     assert_equal(result.errors[0].logMessage, "Number of bedroom elements specified inconsistent with number of bathroom elements specified.")
   end
   
   def test_argument_error_beds_not_equal_to_units
     args_hash = {}
     args_hash["Num_Br"] = "3.0, 3.0"
-    result = _test_error("2000sqft_2story_FB_GRG_UA.osm", args_hash)
+    result = _test_error("singlefamily_detached.osm", args_hash)
     assert_equal(result.errors[0].logMessage, "Number of bedroom elements specified inconsistent with number of multifamily units defined in the model.")
   end
   
   def test_argument_error_baths_not_equal_to_units
     args_hash = {}
     args_hash["Num_Ba"] = "2.0, 2.0"
-    result = _test_error("2000sqft_2story_FB_GRG_UA.osm", args_hash)
+    result = _test_error("singlefamily_detached.osm", args_hash)
     assert_equal(result.errors[0].logMessage, "Number of bathroom elements specified inconsistent with number of multifamily units defined in the model.")
-  end  
+  end
   
   def test_argument_error_beds_not_numerical
     args_hash = {}
     args_hash["Num_Br"] = "3.0, 3.0, typo"
-    result = _test_error("multifamily_3_units.osm", args_hash)
+    result = _test_error("singlefamily_detached.osm", args_hash)
     assert_equal(result.errors[0].logMessage, "Number of bedrooms must be a numerical value.")
   end
   
   def test_argument_error_baths_not_numerical
     args_hash = {}
     args_hash["Num_Ba"] = "2.0, 2.0, typo"
-    result = _test_error("multifamily_3_units.osm", args_hash)
+    result = _test_error("singlefamily_detached.osm", args_hash)
     assert_equal(result.errors[0].logMessage, "Number of bathrooms must be a numerical value.")  
   end
   
   def test_argument_error_beds_not_positive_integer
     args_hash = {}
     args_hash["Num_Br"] = "3.0, 3.0, 3.5"
-    result = _test_error("multifamily_3_units.osm", args_hash)
+    result = _test_error("singlefamily_detached.osm", args_hash)
     assert_equal(result.errors[0].logMessage, "Number of bedrooms must be a positive integer.")    
   end
   
   def test_argument_error_baths_not_positive_multiple_of_0pt25
     args_hash = {}
     args_hash["Num_Ba"] = "2.0, 2.0, 2.8"
-    result = _test_error("multifamily_3_units.osm", args_hash)
+    result = _test_error("singlefamily_detached.osm", args_hash)
     assert_equal(result.errors[0].logMessage, "Number of bathrooms must be a positive multiple of 0.25.")    
   end
   
@@ -65,91 +65,61 @@ class BedroomsAndBathroomsTest < MiniTest::Test
   
   def test_error_inconsistent_units_defined_in_model
     args_hash = {}
-    result = _test_error("multifamily_4_units_listed_3_units_defined.osm", args_hash)
+    result = _test_error("multifamily_8_units_6_listed.osm", args_hash)
     assert_equal(result.errors[0].logMessage, "Cannot determine number of building units; inconsistent number of units defined in the model.")   
    end
   
   def test_error_unit_has_no_spaces
     args_hash = {}
-    result = _test_error("2000sqft_2story_FB_GRG_UA_no_spaces_in_unit.osm", args_hash)
-    assert_equal(result.errors[0].logMessage, "Could not find the space '{59fc425b-e98e-42bf-a6d7-37f9c2809157}' associated with unit 1.")
-  end  
-  
-  def test_sfd_retrofit_replace
-    args_hash = {}
-    model = _test_measure("2000sqft_2story_FB_GRG_UA.osm", args_hash, 3, 2, 1)
-    args_hash = {}
-    args_hash["Num_Br"] = "6.0"
-    args_hash["Num_Ba"] = "4.0"
-    _test_measure(model, args_hash, 6, 4, 1)
+    result = _test_error("multifamily_8_units_missing_space.osm", args_hash)
+    assert_equal(result.errors[0].logMessage, "Could not find the space '{381d5b54-fa3f-4245-9812-ebdc33acae12}' associated with unit 1.")
   end
   
-  def test_mf_retrofit_replace
+  def test_retrofit_replace
     args_hash = {}
-    model = _test_measure("multifamily_3_units.osm", args_hash, 3, 2, 3)
-    args_hash = {}
-    args_hash["Num_Br"] = "6.0"
-    args_hash["Num_Ba"] = "4.0"    
-    _test_measure(model, args_hash, 6, 4, 3)
-  end  
+    expected_num_del_objects = {}
+    expected_num_new_objects = {"ElectricEquipment"=>1, "ElectricEquipmentDefinition"=>1}
+    expected_values = {"Beds"=>3.0, "Baths"=>2.0}
+    model = _test_measure("singlefamily_detached.osm", args_hash, expected_num_del_objects, expected_num_new_objects, expected_values)
+    args_hash["Num_Br"] = "4"
+    args_hash["Num_Ba"] = "3"
+    expected_num_del_objects = {"ElectricEquipment"=>1, "ElectricEquipmentDefinition"=>1}
+    expected_num_new_objects = {"ElectricEquipment"=>1, "ElectricEquipmentDefinition"=>1}
+    expected_values = {"Beds"=>4.0, "Baths"=>3.0}
+    _test_measure(model, args_hash, expected_num_del_objects, expected_num_new_objects, expected_values)    
+  end
   
-  def test_mf_urbanopt_retrofit_replace
+  def test_multifamily_new_construction_1
+    num_units = 4
     args_hash = {}
-    model = _test_measure("multifamily_urbanopt.osm", args_hash, 3, 2, 8)
+    args_hash["Num_Br"] = "4"
+    args_hash["Num_Ba"] = "3"
+    expected_num_del_objects = {}
+    expected_num_new_objects = {"ElectricEquipment"=>1, "ElectricEquipmentDefinition"=>1}
+    expected_values = {"Beds"=>4.0, "Baths"=>3.0}
+    _test_measure("singlefamily_attached_fbsmt_4_units.osm", args_hash, expected_num_del_objects, expected_num_new_objects, expected_values, num_units)    
+  end
+  
+  def test_multifamily_new_construction_2
+    num_units = 8
     args_hash = {}
-    args_hash["Num_Br"] = "6.0"
-    args_hash["Num_Ba"] = "4.0"    
-    _test_measure(model, args_hash, 6, 4, 8)
+    args_hash["Num_Br"] = "4"
+    args_hash["Num_Ba"] = "3"
+    expected_num_del_objects = {}
+    expected_num_new_objects = {"ElectricEquipment"=>1, "ElectricEquipmentDefinition"=>1}
+    expected_values = {"Beds"=>4.0, "Baths"=>3.0}
+    _test_measure("multifamily_8_units.osm", args_hash, expected_num_del_objects, expected_num_new_objects, expected_values, num_units)    
   end  
   
   private
   
-  def _test_error(osm_file, args_hash)
+  def _test_error(osm_file_or_model, args_hash)
     # create an instance of the measure
     measure = AddResidentialBedroomsAndBathrooms.new
 
     # create an instance of a runner
     runner = OpenStudio::Ruleset::OSRunner.new
 
-    model = get_model(File.dirname(__FILE__), osm_file)
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
-
-    # populate argument with specified hash value if specified
-    arguments.each do |arg|
-      temp_arg_var = arg.clone
-      if args_hash[arg.name]
-        assert(temp_arg_var.setValue(args_hash[arg.name]))
-      end
-      argument_map[arg.name] = temp_arg_var
-    end
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-      
-    # assert that it didn't run
-    assert_equal("Fail", result.value.valueName)
-    assert(result.errors.size == 1)
-
-    return result
-    
-  end
-  
-  def _test_measure(osm_file_or_model, args_hash, expected_num_beds=0, expected_num_baths=0, expected_num_units=0)
-    # create an instance of the measure
-    measure = AddResidentialBedroomsAndBathrooms.new
-
-    # check for standard methods
-    assert(!measure.name.empty?)
-    assert(!measure.description.empty?)
-    assert(!measure.modeler_description.empty?)
-
-    # create an instance of a runner
-    runner = OpenStudio::Ruleset::OSRunner.new
-    
     model = get_model(File.dirname(__FILE__), osm_file_or_model)
 
     # get arguments
@@ -169,16 +139,76 @@ class BedroomsAndBathroomsTest < MiniTest::Test
     measure.run(model, runner, argument_map)
     result = runner.result
 
+    # assert that it didn't run
+    assert_equal("Fail", result.value.valueName)
+    assert(result.errors.size == 1)
+    
+    return result
+  end  
+  
+  def _test_measure(osm_file_or_model, args_hash, expected_num_del_objects, expected_num_new_objects, expected_values, num_infos=0, num_warnings=0, debug=false)
+    # create an instance of the measure
+    measure = AddResidentialBedroomsAndBathrooms.new
+
+    # check for standard methods
+    assert(!measure.name.empty?)
+    assert(!measure.description.empty?)
+    assert(!measure.modeler_description.empty?)
+
+    # create an instance of a runner
+    runner = OpenStudio::Ruleset::OSRunner.new
+    
+    model = get_model(File.dirname(__FILE__), osm_file_or_model)
+
+    # get the initial objects in the model
+    initial_objects = get_objects_as_strings(model)
+   
+    
+    # get arguments
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
+
+    # populate argument with specified hash value if specified
+    arguments.each do |arg|
+      temp_arg_var = arg.clone
+      if args_hash[arg.name]
+        assert(temp_arg_var.setValue(args_hash[arg.name]))
+      end
+      argument_map[arg.name] = temp_arg_var
+    end
+    
+    # run the measure
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    
     # assert that it ran correctly
     assert_equal("Success", result.value.valueName)
-
-    if expected_num_units > 1
-      assert_equal(result.finalCondition.get.logMessage, "The building has been assigned #{(expected_num_beds*expected_num_units).round(1)} bedroom(s) and #{(expected_num_baths*expected_num_units).round(1)} bathroom(s) across #{expected_num_units} units.")
-    else
-      assert_equal(result.finalCondition.get.logMessage, "The building has been assigned #{(expected_num_beds*expected_num_units).round(1)} bedroom(s) and #{(expected_num_baths*expected_num_units).round(1)} bathroom(s).")
+    assert(result.info.size == num_infos)
+    assert(result.warnings.size == num_warnings)
+    
+    # get the final objects in the model  
+    final_objects = get_objects_as_strings(model)
+    
+    # get new and deleted objects
+    obj_type_exclusions = []
+    all_new_objects = get_object_additions(initial_objects, final_objects, obj_type_exclusions)
+    all_del_objects = get_object_additions(final_objects, initial_objects, obj_type_exclusions)
+    
+    # check we have the expected number of new/deleted objects
+    check_num_objects(all_new_objects, expected_num_new_objects, "added")
+    check_num_objects(all_del_objects, expected_num_del_objects, "deleted")
+    
+    all_new_objects.each do |obj_type, new_objects|
+        new_objects.each do |new_object|
+            if obj_type == "ElectricEquipment"
+                assert(new_object.split(", !- Name")[0].split(", !- Handle")[1].include?("bed=#{expected_values["Beds"]}|bath=#{expected_values["Baths"]}"))
+            elsif obj_type == "ElectricEquipmentDefinition"
+                assert(new_object.split(", !- Name")[0].split(", !- Handle")[1].include?("bed=#{expected_values["Beds"]}|bath=#{expected_values["Baths"]}"))
+            end
+        end
     end
-
+    
     return model
-  end  
+  end 
   
 end
