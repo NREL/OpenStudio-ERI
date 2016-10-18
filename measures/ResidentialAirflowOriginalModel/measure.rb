NREL/OpenStudio-ERI

commit 29ffcb27557ecf44791985cc19bcd0018eebf03a

Update measures to use new BuildingUnit object. Addresses #101 and #29.

measures/ResidentialAirflowOriginalModel/measure.rb
@@ -925,8 +925,9 @@ class ProcessAirflowOriginalModel < OpenStudio::Ruleset::WorkspaceUserScript
       hasForcedAirEquipment = true
     end
 
-    num_units = Geometry.get_num_units(model, runner)
-    if num_units.nil?
+    # Get building units
+    units = Geometry.get_building_units(model, runner)
+    if units.nil?
         return false
     end
 
@@ -940,7 +941,7 @@ class ProcessAirflowOriginalModel < OpenStudio::Ruleset::WorkspaceUserScript
     end
     geometry.building_height = Geometry.get_building_height(model.getSpaces)
     geometry.stories = Geometry.get_building_stories(model.getSpaces)
-    geometry.num_units = num_units
+    geometry.num_units = units.size
     geometry.above_grade_volume = Geometry.get_above_grade_finished_volume_from_spaces(model.getSpaces, true)
     geometry.above_grade_exterior_wall_area = Geometry.calculate_exterior_wall_area(model.getSpaces, false)
     
@@ -1000,16 +1001,13 @@ class ProcessAirflowOriginalModel < OpenStudio::Ruleset::WorkspaceUserScript
     
     duct_locations = {}
     
-    (1..num_units).to_a.each do |unit_num|
-      nbeds, nbaths, unit_spaces = Geometry.get_unit_beds_baths_spaces(model, unit_num, runner)
-      if unit_spaces.nil?
-        runner.registerError("Could not determine the spaces associated with unit #{unit_num}.")
-        return false
-      end
+    units.each_with_index do |building_unit, unit_index|
+      nbeds, nbaths = Geometry.get_unit_beds_baths(model, building_unit, runner)
       if nbeds.nil? or nbaths.nil?
-        runner.registerError("Could not determine number of bedrooms or bathrooms. Run the 'Add Residential Bedrooms And Bathrooms' measure first.")
         return false
       end
+      unit_spaces = building_unit.spaces
+      unit_num = unit_index+1
 
       unit = Unit.new
       unit.num_bedrooms = nbeds
@@ -1019,7 +1017,7 @@ class ProcessAirflowOriginalModel < OpenStudio::Ruleset::WorkspaceUserScript
       unit.finished_floor_area = Geometry.get_finished_floor_area_from_spaces(unit_spaces, false, runner)
       thermal_zones = Geometry.get_thermal_zones_from_spaces(unit_spaces)
       if thermal_zones.length > 1
-        runner.registerInfo("Unit #{unit_num} spans more than one thermal zone.")
+        runner.registerInfo("#{building_unit.name.to_s} spans more than one thermal zone.")
       end
       
       living_space = LivingSpace.new
@@ -1072,7 +1070,7 @@ class ProcessAirflowOriginalModel < OpenStudio::Ruleset::WorkspaceUserScript
       end
       unit_dryer_exhaust = dryerExhaust
       if not has_cd and dryerExhaust > 0
-        runner.registerWarning("No clothes dryer object was found in unit #{unit_num} but the clothes dryer exhaust specified is non-zero. Overriding clothes dryer exhaust to be zero.")
+        runner.registerWarning("No clothes dryer object was found in #{building_unit.name.to_s} but the clothes dryer exhaust specified is non-zero. Overriding clothes dryer exhaust to be zero.")
         unit_dryer_exhaust = 0
       end
       
@@ -2579,8 +2577,9 @@ class ProcessAirflowOriginalModel < OpenStudio::Ruleset::WorkspaceUserScript
       runner.registerInfo("Set object '#{str.split("\n")[1].gsub(",","")} - #{str.split("\n")[2].split(",")[0]}'")
     end    
         
-    (1..num_units).to_a.each do |unit_num|
-      _nbeds, _nbaths, unit_spaces = Geometry.get_unit_beds_baths_spaces(model, unit_num, runner)
+    units.each_with_index do |building_unit, unit_index|
+      unit_num = unit_index+1
+      unit_spaces = building_unit.spaces
       thermal_zones = Geometry.get_thermal_zones_from_spaces(unit_spaces)      
       living_thermal_zone_name = nil
       thermal_zones.each do |thermal_zone|

measures/ResidentialAirflowOriginalModel/measure.xml
@@ -2,8 +2,8 @@
   <schema_version>3.0</schema_version>
   <name>process_airflow_original_model</name>
   <uid>5a7ae205-bdc2-4daf-b29e-b6f0a59366a6</uid>
-  <version_id>e658bef1-3ce0-4830-8828-3a4cab045c5d</version_id>
-  <version_modified>20161017T165717Z</version_modified>
+  <version_id>245dab45-7a05-4102-a635-0594da91299d</version_id>
+  <version_modified>20161017T233836Z</version_modified>
   <xml_checksum>2AC0F979</xml_checksum>
   <class_name>ProcessAirflowOriginalModel</class_name>
   <display_name>Set Residential Airflow - Original Model</display_name>
@@ -570,29 +570,6 @@
       <checksum>97B219DD</checksum>
     </file>
     <file>
-      <version>
-        <software_program>OpenStudio</software_program>
-        <identifier>1.4.0</identifier>
-        <min_compatible>1.12.5</min_compatible>
-      </version>
-      <filename>measure.rb</filename>
-      <filetype>rb</filetype>
-      <usage_type>script</usage_type>
-      <checksum>2152F9B1</checksum>
-    </file>
-    <file>
-      <filename>geometry.rb</filename>
-      <filetype>rb</filetype>
-      <usage_type>resource</usage_type>
-      <checksum>3BCBA48E</checksum>
-    </file>
-    <file>
-      <filename>constants.rb</filename>
-      <filetype>rb</filetype>
-      <usage_type>resource</usage_type>
-      <checksum>838E9F01</checksum>
-    </file>
-    <file>
       <filename>util.rb</filename>
       <filetype>rb</filetype>
       <usage_type>resource</usage_type>
@@ -611,58 +588,81 @@
       <checksum>5E54715B</checksum>
     </file>
     <file>
+      <version>
+        <software_program>OpenStudio</software_program>
+        <identifier>1.4.0</identifier>
+        <min_compatible>1.13.1</min_compatible>
+      </version>
+      <filename>measure.rb</filename>
+      <filetype>rb</filetype>
+      <usage_type>script</usage_type>
+      <checksum>A2F87B72</checksum>
+    </file>
+    <file>
+      <filename>geometry.rb</filename>
+      <filetype>rb</filetype>
+      <usage_type>resource</usage_type>
+      <checksum>7326234C</checksum>
+    </file>
+    <file>
+      <filename>constants.rb</filename>
+      <filetype>rb</filetype>
+      <usage_type>resource</usage_type>
+      <checksum>9195CA47</checksum>
+    </file>
+    <file>
       <filename>singlefamily_slab_location_neighbors_beds_furnace_central_air_conditioner.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>2EEBAEFE</checksum>
+      <checksum>0F22D05E</checksum>
     </file>
     <file>
       <filename>singlefamily_slab_location_beds_electric_baseboard.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>A77A4AF7</checksum>
+      <checksum>276AA862</checksum>
     </file>
     <file>
       <filename>singlefamily_slab_location_beds_furnace_clothes_dryer.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>700F9C6F</checksum>
+      <checksum>121B5E15</checksum>
     </file>
     <file>
       <filename>singlefamily_garage_location_beds_furnace_central_air_conditioner.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>13357FC6</checksum>
+      <checksum>77A8E523</checksum>
     </file>
     <file>
       <filename>singlefamily_ufbasement_location_beds_furnace_central_air_conditioner.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>786DDAD4</checksum>
+      <checksum>8CC69F28</checksum>
     </file>
     <file>
       <filename>singlefamily_crawl_location_beds_furnace_central_air_conditioner.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>7FB154D3</checksum>
+      <checksum>41DEFBCF</checksum>
     </file>
     <file>
       <filename>singlefamily_fbasement_location_beds_furnace_central_air_conditioner.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>C00D9885</checksum>
+      <checksum>458AE32D</checksum>
     </file>
     <file>
       <filename>singlefamily_slab_location_beds_mshp.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>4A53C616</checksum>
+      <checksum>80294C62</checksum>
     </file>
     <file>
       <filename>singlefamily_slab_location_beds_furnace_central_air_conditioner.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>9F6356E9</checksum>
+      <checksum>CD7FC9A3</checksum>
     </file>
   </files>
 </measure>

measures/ResidentialAirflowOriginalModel/tests/singlefamily_crawl_location_beds_furnace_central_air_conditioner.osm
@@ -1,7 +1,7 @@
 
 OS:Version,
   {fc003c06-618b-4051-9b37-6b6f71b12672}, !- Handle
-  1.12.2;                                 !- Version Identifier
+  1.13.1;                                 !- Version Identifier
 
 OS:ClimateZones,
   {135edfab-17e7-40db-a436-27d385a8cd6d}, !- Handle
@@ -382,7 +382,10 @@ OS:Space,
   0,                                      !- Y Origin {m}
   0,                                      !- Z Origin {m}
   ,                                       !- Building Story Name
-  {2aed6f47-1f16-4f95-8755-98bfa36d35cd}; !- Thermal Zone Name
+  {2aed6f47-1f16-4f95-8755-98bfa36d35cd}, !- Thermal Zone Name
+  ,                                       !- Part of Total Floor Area
+  ,                                       !- Design Specification Outdoor Air Object Name
+  {90e9ea70-7f35-475a-921b-d0af51452439}; !- Building Unit Name
 
 OS:Surface,
   {dfff93d7-4e22-448a-83ba-6de2b8ed0952}, !- Handle
@@ -497,7 +500,10 @@ OS:Space,
   0,                                      !- Y Origin {m}
   2.4384,                                 !- Z Origin {m}
   ,                                       !- Building Story Name
-  {2aed6f47-1f16-4f95-8755-98bfa36d35cd}; !- Thermal Zone Name
+  {2aed6f47-1f16-4f95-8755-98bfa36d35cd}, !- Thermal Zone Name
+  ,                                       !- Part of Total Floor Area
+  ,                                       !- Design Specification Outdoor Air Object Name
+  {90e9ea70-7f35-475a-921b-d0af51452439}; !- Building Unit Name
 
 OS:Surface,
   {d4facccc-dac7-470f-814a-f71a2bfb6e68}, !- Handle
@@ -695,7 +701,10 @@ OS:Space,
   0,                                      !- Y Origin {m}
   4.8768,                                 !- Z Origin {m}
   ,                                       !- Building Story Name
-  {5dfce048-b7ff-4448-83b9-0855250535fd}; !- Thermal Zone Name
+  {5dfce048-b7ff-4448-83b9-0855250535fd}, !- Thermal Zone Name
+  ,                                       !- Part of Total Floor Area
+  ,                                       !- Design Specification Outdoor Air Object Name
+  {90e9ea70-7f35-475a-921b-d0af51452439}; !- Building Unit Name
 
 OS:ThermalZone,
   {5dfce048-b7ff-4448-83b9-0855250535fd}, !- Handle
@@ -872,7 +881,10 @@ OS:Space,
   0,                                      !- Y Origin {m}
   -1.2192,                                !- Z Origin {m}
   ,                                       !- Building Story Name
-  {811ede10-f108-44a8-852b-dc2798dc5472}; !- Thermal Zone Name
+  {811ede10-f108-44a8-852b-dc2798dc5472}, !- Thermal Zone Name
+  ,                                       !- Part of Total Floor Area
+  ,                                       !- Design Specification Outdoor Air Object Name
+  {90e9ea70-7f35-475a-921b-d0af51452439}; !- Building Unit Name
 
 OS:Surface,
   {ecabf5b3-9373-421a-8bee-9e32ff44b7ed}, !- Handle
@@ -976,35 +988,17 @@ OS:Surface,
   0, 6.81553519541936, 1.2192,            !- X,Y,Z Vertex 3 {m}
   0, 0, 1.2192;                           !- X,Y,Z Vertex 4 {m}
 
-OS:ElectricEquipment:Definition,
-  {6cca597b-3506-4aae-81d8-e2d1fd8281b3}, !- Handle
-  unit=1|bed=3.0|bath=2.0|space={b8668386-dd32-41f6-8820-7a194fff70ee}|space={cf17e2e3-09e7-495a-a799-4bf9cfa4d8d9}, !- Name
-  EquipmentLevel,                         !- Design Level Calculation Method
-  0,                                      !- Design Level {W}
-  ,                                       !- Watts per Space Floor Area {W/m2}
-  ;                                       !- Watts per Person {W/person}
-
-OS:Schedule:Ruleset,
-  {6fe58715-68f4-4658-9177-bb66fe4732c6}, !- Handle
-  empty_schedule,                         !- Name
-  {bfa37987-a635-40f8-87e7-1036fd640aa3}, !- Schedule Type Limits Name
-  {cafadac7-4dd3-4350-b27d-17b22ee22847}; !- Default Day Schedule Name
-
-OS:Schedule:Day,
-  {cafadac7-4dd3-4350-b27d-17b22ee22847}, !- Handle
-  Schedule Day 1,                         !- Name
-  {bfa37987-a635-40f8-87e7-1036fd640aa3}, !- Schedule Type Limits Name
-  ,                                       !- Interpolate to Timestep
-  24,                                     !- Hour 1
-  0,                                      !- Minute 1
-  0;                                      !- Value Until Time 1
-
-OS:ElectricEquipment,
-  {fc0bd3e9-8d40-4e20-967d-da4b4e6866cd}, !- Handle
-  unit=1|bed=3.0|bath=2.0|space={b8668386-dd32-41f6-8820-7a194fff70ee}|space={cf17e2e3-09e7-495a-a799-4bf9cfa4d8d9}, !- Name
-  {6cca597b-3506-4aae-81d8-e2d1fd8281b3}, !- Electric Equipment Definition Name
-  ,                                       !- Space or SpaceType Name
-  {6fe58715-68f4-4658-9177-bb66fe4732c6}; !- Schedule Name
+OS:BuildingUnit,
+  {90e9ea70-7f35-475a-921b-d0af51452439}, !- Handle
+  unit 1,                                 !- Name
+  ,                                       !- Rendering Color
+  Residential,                            !- Building Unit Type
+  NumberOfBedrooms,                       !- Building Unit Feature Name 1
+  Integer,                                !- Building Unit Feature Data Type 1
+  3,                                      !- Building Unit Feature Value 1
+  NumberOfBathrooms,                      !- Building Unit Feature Name 2
+  Double,                                 !- Building Unit Feature Data Type 2
+  2;                                      !- Building Unit Feature Value 2
 
 OS:ScheduleTypeLimits,
   {bfa37987-a635-40f8-87e7-1036fd640aa3}, !- Handle

commit 5d3ee0a94b035c8370a4cbd3bcad084eeb2d4efa

Bugfix for airflow EnergyPlus measure. Bug introduced in https://github.com/NREL/OpenStudio-BEopt/commit/29ffcb27557ecf44791985cc19bcd0018eebf03a.

measures/ResidentialAirflowOriginalModel/measure.rb
@@ -2013,10 +2013,14 @@ class ProcessAirflowOriginalModel < OpenStudio::Ruleset::WorkspaceUserScript
         demand_side_outlet_node_name = nil
         demand_side_inlet_node_names = nil
         workspace.getObjectsByType("AirLoopHVAC".to_IddObjectType).each do |airloop|
-          if airloop.getString(0).to_s.include? "Central Air System_#{unit_num}"
+          if airloop.getString(0).to_s.include? "Central Air System_unit #{unit_num}"
             demand_side_outlet_node_name = airloop.getString(7).to_s
             demand_side_inlet_node_names = airloop.getString(8).to_s
           end
+        end
+        if demand_side_outlet_node_name.nil?
+          runner.registerError("Could not find AirLoopHVAC demand side outlet node name.")
+          return false
         end
         
         demand_side_inlet_node_name = nil
@@ -2024,6 +2028,10 @@ class ProcessAirflowOriginalModel < OpenStudio::Ruleset::WorkspaceUserScript
           if nodelist.getString(0).to_s == demand_side_inlet_node_names
             demand_side_inlet_node_name = nodelist.getString(1).to_s
           end
+        end
+        if demand_side_inlet_node_name.nil?
+          runner.registerError("Could not find AirLoopHVAC demand side inlet node name.")
+          return false
         end
               
         # Return Air

measures/ResidentialAirflowOriginalModel/measure.xml
@@ -2,8 +2,8 @@
   <schema_version>3.0</schema_version>
   <name>process_airflow_original_model</name>
   <uid>5a7ae205-bdc2-4daf-b29e-b6f0a59366a6</uid>
-  <version_id>245dab45-7a05-4102-a635-0594da91299d</version_id>
-  <version_modified>20161017T233836Z</version_modified>
+  <version_id>146e6df5-379c-496f-9931-7a5b606ff71f</version_id>
+  <version_modified>20161018T170941Z</version_modified>
   <xml_checksum>2AC0F979</xml_checksum>
   <class_name>ProcessAirflowOriginalModel</class_name>
   <display_name>Set Residential Airflow - Original Model</display_name>
@@ -588,17 +588,6 @@
       <checksum>5E54715B</checksum>
     </file>
     <file>
-      <version>
-        <software_program>OpenStudio</software_program>
-        <identifier>1.4.0</identifier>
-        <min_compatible>1.13.1</min_compatible>
-      </version>
-      <filename>measure.rb</filename>
-      <filetype>rb</filetype>
-      <usage_type>script</usage_type>
-      <checksum>A2F87B72</checksum>
-    </file>
-    <file>
       <filename>geometry.rb</filename>
       <filetype>rb</filetype>
       <usage_type>resource</usage_type>
@@ -611,46 +600,10 @@
       <checksum>9195CA47</checksum>
     </file>
     <file>
-      <filename>singlefamily_slab_location_neighbors_beds_furnace_central_air_conditioner.osm</filename>
-      <filetype>osm</filetype>
-      <usage_type>test</usage_type>
-      <checksum>0F22D05E</checksum>
-    </file>
-    <file>
       <filename>singlefamily_slab_location_beds_electric_baseboard.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
       <checksum>276AA862</checksum>
-    </file>
-    <file>
-      <filename>singlefamily_slab_location_beds_furnace_clothes_dryer.osm</filename>
-      <filetype>osm</filetype>
-      <usage_type>test</usage_type>
-      <checksum>121B5E15</checksum>
-    </file>
-    <file>
-      <filename>singlefamily_garage_location_beds_furnace_central_air_conditioner.osm</filename>
-      <filetype>osm</filetype>
-      <usage_type>test</usage_type>
-      <checksum>77A8E523</checksum>
-    </file>
-    <file>
-      <filename>singlefamily_ufbasement_location_beds_furnace_central_air_conditioner.osm</filename>
-      <filetype>osm</filetype>
-      <usage_type>test</usage_type>
-      <checksum>8CC69F28</checksum>
-    </file>
-    <file>
-      <filename>singlefamily_crawl_location_beds_furnace_central_air_conditioner.osm</filename>
-      <filetype>osm</filetype>
-      <usage_type>test</usage_type>
-      <checksum>41DEFBCF</checksum>
-    </file>
-    <file>
-      <filename>singlefamily_fbasement_location_beds_furnace_central_air_conditioner.osm</filename>
-      <filetype>osm</filetype>
-      <usage_type>test</usage_type>
-      <checksum>458AE32D</checksum>
     </file>
     <file>
       <filename>singlefamily_slab_location_beds_mshp.osm</filename>
@@ -659,10 +612,57 @@
       <checksum>80294C62</checksum>
     </file>
     <file>
+      <filename>singlefamily_slab_location_neighbors_beds_furnace_central_air_conditioner.osm</filename>
+      <filetype>osm</filetype>
+      <usage_type>test</usage_type>
+      <checksum>134501A8</checksum>
+    </file>
+    <file>
+      <filename>singlefamily_slab_location_beds_furnace_clothes_dryer.osm</filename>
+      <filetype>osm</filetype>
+      <usage_type>test</usage_type>
+      <checksum>6AFD837B</checksum>
+    </file>
+    <file>
+      <filename>singlefamily_garage_location_beds_furnace_central_air_conditioner.osm</filename>
+      <filetype>osm</filetype>
+      <usage_type>test</usage_type>
+      <checksum>9C1DD6E3</checksum>
+    </file>
+    <file>
+      <filename>singlefamily_ufbasement_location_beds_furnace_central_air_conditioner.osm</filename>
+      <filetype>osm</filetype>
+      <usage_type>test</usage_type>
+      <checksum>6773ACE8</checksum>
+    </file>
+    <file>
+      <filename>singlefamily_crawl_location_beds_furnace_central_air_conditioner.osm</filename>
+      <filetype>osm</filetype>
+      <usage_type>test</usage_type>
+      <checksum>AA6BC80F</checksum>
+    </file>
+    <file>
+      <filename>singlefamily_fbasement_location_beds_furnace_central_air_conditioner.osm</filename>
+      <filetype>osm</filetype>
+      <usage_type>test</usage_type>
+      <checksum>0D501959</checksum>
+    </file>
+    <file>
       <filename>singlefamily_slab_location_beds_furnace_central_air_conditioner.osm</filename>
       <filetype>osm</filetype>
       <usage_type>test</usage_type>
-      <checksum>CD7FC9A3</checksum>
+      <checksum>26CAFA63</checksum>
+    </file>
+    <file>
+      <version>
+        <software_program>OpenStudio</software_program>
+        <identifier>1.4.0</identifier>
+        <min_compatible>1.13.1</min_compatible>
+      </version>
+      <filename>measure.rb</filename>
+      <filetype>rb</filetype>
+      <usage_type>script</usage_type>
+      <checksum>EDA92C3F</checksum>
     </file>
   </files>
 </measure>

measures/ResidentialAirflowOriginalModel/tests/singlefamily_crawl_location_beds_furnace_central_air_conditioner.osm
@@ -4313,7 +4313,7 @@ OS:AirLoopHVAC:UnitarySystem,
 
 OS:AirLoopHVAC,
   {54ea28c8-a56c-402e-aeb2-73d2bbab5269}, !- Handle
-  Central Air System_1,                   !- Name
+  Central Air System_unit 1,              !- Name
   ,                                       !- Controller List Name
   {c4aa7a2b-1705-42c6-9e6a-da2954b05d30}, !- Availability Schedule
   ,                                       !- Availability Manager

measures/ResidentialAirflowOriginalModel/tests/singlefamily_fbasement_location_beds_furnace_central_air_conditioner.osm
@@ -4635,7 +4635,7 @@ OS:AirLoopHVAC:UnitarySystem,
 
 OS:AirLoopHVAC,
   {71252e3c-6e2a-4549-9bbc-7d8daaa7279b}, !- Handle
-  Central Air System_1,                   !- Name
+  Central Air System_unit 1,              !- Name
   ,                                       !- Controller List Name
   {233866f6-c77b-4523-a28c-46810ca1d021}, !- Availability Schedule
   ,                                       !- Availability Manager

measures/ResidentialAirflowOriginalModel/tests/singlefamily_garage_location_beds_furnace_central_air_conditioner.osm
@@ -4521,7 +4521,7 @@ OS:AirLoopHVAC:UnitarySystem,
 
 OS:AirLoopHVAC,
   {ba0c48bf-f3f1-4f66-8b2f-e9f4a6414aeb}, !- Handle
-  Central Air System_1,                   !- Name
+  Central Air System_unit 1,              !- Name
   ,                                       !- Controller List Name
   {2f9c4ee8-de3a-4126-82da-f859729c2ee9}, !- Availability Schedule
   ,                                       !- Availability Manager

measures/ResidentialAirflowOriginalModel/tests/singlefamily_slab_location_beds_furnace_central_air_conditioner.osm
@@ -4113,7 +4113,7 @@ OS:AirLoopHVAC:UnitarySystem,
 
 OS:AirLoopHVAC,
   {c996bf09-33f6-427f-8caa-92bc8c22153c}, !- Handle
-  Central Air System_1,                   !- Name
+  Central Air System_unit 1,              !- Name
   ,                                       !- Controller List Name
   {7d124814-98c1-4133-abc3-5702a79a91e4}, !- Availability Schedule
   ,                                       !- Availability Manager

measures/ResidentialAirflowOriginalModel/tests/singlefamily_slab_location_beds_furnace_clothes_dryer.osm
@@ -875,7 +875,7 @@ OS:ScheduleTypeLimits,
 
 OS:AirLoopHVAC,
   {4db52d77-7b14-48d8-b8c9-8e40160b71bd}, !- Handle
-  Central Air System_1,                   !- Name
+  Central Air System_unit 1,              !- Name
   ,                                       !- Controller List Name
   {8ff48d8e-75df-4d73-b906-5e8657ca2cf3}, !- Availability Schedule
   ,                                       !- Availability Manager

measures/ResidentialAirflowOriginalModel/tests/singlefamily_slab_location_neighbors_beds_furnace_central_air_conditioner.osm
@@ -1330,7 +1330,7 @@ OS:AirLoopHVAC:UnitarySystem,
 
 OS:AirLoopHVAC,
   {ab3a47b9-4c82-4c7e-9609-14c1642b1bba}, !- Handle
-  Central Air System_1,                   !- Name
+  Central Air System_unit 1,              !- Name
   ,                                       !- Controller List Name
   {a3868c64-187b-402a-b8ac-49dfd44f607f}, !- Availability Schedule
   ,                                       !- Availability Manager

measures/ResidentialAirflowOriginalModel/tests/singlefamily_ufbasement_location_beds_furnace_central_air_conditioner.osm
@@ -4313,7 +4313,7 @@ OS:AirLoopHVAC:UnitarySystem,
 
 OS:AirLoopHVAC,
   {f7526c36-14d3-4dc3-85c5-ff3b8cbf26fa}, !- Handle
-  Central Air System_1,                   !- Name
+  Central Air System_unit 1,              !- Name
   ,                                       !- Controller List Name
   {ee5cd2c3-e5d6-40a8-9410-5951e0e65e3c}, !- Availability Schedule
   ,                                       !- Availability Manager