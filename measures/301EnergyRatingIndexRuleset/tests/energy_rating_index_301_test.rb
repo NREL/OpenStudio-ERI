648cef94b5229a40e5f4f60f6e809f2905439547
Scott Horowitz <scott.horowitz@nrel.gov>
2017-10-16 09:23:05 -0600

    Add unit tests for heating systems.

--- measures/301EnergyRatingIndexRuleset/tests/energy_rating_index_301_test.rb
+++ measures/301EnergyRatingIndexRuleset/tests/energy_rating_index_301_test.rb
@@ -7,6 +7,273 @@ require 'fileutils'
 
 class EnergyRatingIndex301Test < MiniTest::Test
 
+  '''
+  Test reference/rated home logic via HPXML text snippets.
+  '''
+  
+  def get_hpxml_doc_details
+    hpxml = """
+      <?xml version='1.0' encoding='UTF-8'?>
+      <HPXML xmlns='http://hpxmlonline.com/2014/6' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://hpxmlonline.com/2014/6 N:/Groups/REM/XML/BPI HPXML Standard/schemas/HPXML.xsd' schemaVersion='2.2'>
+        <Building>
+          <BuildingDetails>
+          </BuildingDetails>
+        </Building>
+      </HPXML>
+    """
+    hpxml_doc = REXML::Document.new(hpxml)
+    details = hpxml_doc.elements["//Building/BuildingDetails"]
+    return details
+  end
+
+  def test_hvac_heating_systems
+
+    '''
+    Air-to-air heat pump
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    hp = XMLHelper.add_element(details, "Systems/HVAC/HVACPlant/HeatPump")
+    XMLHelper.add_element(hp, "HeatPumpType", "air-to-air")
+
+    # Reference Home: Air-to-air heat pump, HSPF 7.7
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    assert_equal(7.7, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/AnnualHeatEfficiency[Units='HSPF']/Value")))
+    
+    # Rated Home: Air-to-air heat pump
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    
+    '''
+    Ground-to-air heat pump
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    hp = XMLHelper.add_element(details, "Systems/HVAC/HVACPlant/HeatPump")
+    XMLHelper.add_element(hp, "HeatPumpType", "ground-to-air")
+
+    # Reference Home: Air-to-air heat pump, HSPF 7.7
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    assert_equal(7.7, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/AnnualHeatEfficiency[Units='HSPF']/Value")))
+    
+    # Rated Home: Ground-to-air heat pump
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("ground-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    
+    '''
+    Mini-split heat pump
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    hp = XMLHelper.add_element(details, "Systems/HVAC/HVACPlant/HeatPump")
+    XMLHelper.add_element(hp, "HeatPumpType", "mini-split")
+
+    # Reference Home: Air-to-air heat pump, HSPF 7.7
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    assert_equal(7.7, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/AnnualHeatEfficiency[Units='HSPF']/Value")))
+    
+    # Rated Home: Mini-split heat pump
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("mini-split", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    
+    '''
+    Natural gas furnace
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    hs = XMLHelper.add_element(details, "Systems/HVAC/HVACPlant/HeatingSystem")
+    XMLHelper.add_element(hs, "HeatingSystemType/Furnace")
+    XMLHelper.add_element(hs, "HeatingSystemFuel", "natural gas")
+    
+    # Reference Home: Natural gas furnace, AFUE 78
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/Furnace"))
+    assert_equal("natural gas", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    assert_equal(0.78, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/AnnualHeatingEfficiency[Units='AFUE']/Value")))
+    
+    # Rated Home: Natural gas furnace
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/Furnace"))
+    assert_equal("natural gas", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    
+    '''
+    Electric furnace
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    hs = XMLHelper.add_element(details, "Systems/HVAC/HVACPlant/HeatingSystem")
+    XMLHelper.add_element(hs, "HeatingSystemType/Furnace")
+    XMLHelper.add_element(hs, "HeatingSystemFuel", "electricity")
+    
+    # Reference Home: Air-to-air heat pump, HSPF 7.7
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    assert_equal(7.7, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/AnnualHeatEfficiency[Units='HSPF']/Value")))
+    
+    # Rated Home: Electric furnace
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/Furnace"))
+    assert_equal("electricity", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    
+    '''
+    Natural gas boiler
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    hs = XMLHelper.add_element(details, "Systems/HVAC/HVACPlant/HeatingSystem")
+    XMLHelper.add_element(hs, "HeatingSystemType/Boiler")
+    XMLHelper.add_element(hs, "HeatingSystemFuel", "natural gas")
+    
+    # Reference Home: Natural gas boiler, AFUE 80
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/Boiler"))
+    assert_equal("natural gas", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    assert_equal(0.80, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/AnnualHeatingEfficiency[Units='AFUE']/Value")))
+
+    # Rated Home: Natural gas boiler
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/Boiler"))
+    assert_equal("natural gas", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    
+    '''
+    Electric boiler
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    hs = XMLHelper.add_element(details, "Systems/HVAC/HVACPlant/HeatingSystem")
+    XMLHelper.add_element(hs, "HeatingSystemType/Boiler")
+    XMLHelper.add_element(hs, "HeatingSystemFuel", "electricity")
+    
+    # Reference Home: Air-to-air heat pump, HSPF 7.7
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    assert_equal(7.7, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/AnnualHeatEfficiency[Units='HSPF']/Value")))
+    
+    # Rated Home: Electric boiler
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/Boiler"))
+    assert_equal("electricity", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    
+    '''
+    Electric resistance
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    hs = XMLHelper.add_element(details, "Systems/HVAC/HVACPlant/HeatingSystem")
+    XMLHelper.add_element(hs, "HeatingSystemType/ElectricResistance")
+    XMLHelper.add_element(hs, "HeatingSystemFuel", "electricity")
+    
+    # Reference Home: Air-to-air heat pump, HSPF 7.7
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    assert_equal(7.7, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/AnnualHeatEfficiency[Units='HSPF']/Value")))
+    
+    # Rated Home: Electric resistance
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/ElectricResistance"))
+    assert_equal("electricity", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    
+    '''
+    None (gas available)
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    XMLHelper.add_element(fta, "Fuel", "natural gas")
+    
+    # Reference Home: Natural gas furnace, AFUE 78
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/Furnace"))
+    assert_equal("natural gas", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    assert_equal(0.78, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/AnnualHeatingEfficiency[Units='AFUE']/Value")))
+    
+    # Rated Home: Natural gas furnace
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemType/Furnace"))
+    assert_equal("natural gas", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatingSystem/HeatingSystemFuel"))
+    
+    '''
+    None (gas not available)
+    '''
+    
+    details = get_hpxml_doc_details
+    fta = XMLHelper.add_element(details, "BuildingSummary/Site/FuelTypesAvailable")
+    XMLHelper.add_element(fta, "Fuel", "electricity")
+    
+    # Reference Home: Air-to-air heat pump, HSPF 7.7
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_reference(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    assert_equal(7.7, Float(XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/AnnualHeatEfficiency[Units='HSPF']/Value")))
+    
+    # Rated Home: Air-to-air heat pump
+    systems = REXML::Element.new
+    EnergyRatingIndex301Ruleset.set_systems_hvac_rated(systems, details)
+    assert(XMLHelper.has_element(systems, "HVAC/HVACPlant/HeatPump"))
+    assert_equal("air-to-air", XMLHelper.get_value(systems, "HVAC/HVACPlant/HeatPump/HeatPumpType"))
+    
+  end
+  
+  '''
+  Test running simulations for various HPXML files to completion.
+  '''
+  
   def get_args_hash(hpxml_filename, calc_type)
     args_hash = {}
     args_hash["hpxml_file_path"] = "../../workflow/sample_files/#{hpxml_filename}"
