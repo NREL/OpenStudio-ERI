10b435eb087657737df00960452b525a097bca51
Joe Robertson <joseph.robertson@nrel.gov>
2018-08-08 14:15:55 -0600

    Fix debug option for energy_rating_index.

--- workflow/design.rb
+++ workflow/design.rb
@@ -44,9 +44,7 @@ def create_idf(design, basedir, resultsdir, hpxml, debug)
   #args['schemas_dir'] = File.absolute_path(File.join(basedir, "..", "hpxml_schemas"))
   args['hpxml_output_path'] = output_hpxml_path
   args['epw_output_path'] = File.join(rundir, "in.epw")
-  if debug
-    args['osm_output_path'] = output_hpxml_path.gsub(".xml",".osm")
-  end
+  args['debug'] = debug
   
   update_args_hash(measures, measure_subdir, args)
   success = apply_measures(measures_dir, measures, runner, model, nil, nil, false)
@@ -87,6 +85,6 @@ if ARGV.size == 5
   design = ARGV[1]
   resultsdir = ARGV[2]
   hpxml = ARGV[3]
-  debug = (ARGV[4].downcase.to_s == "true")
+  debug = ARGV[4]
   run_design(basedir, design, resultsdir, hpxml, debug)
 end
