07bb2160071d1e573ed3bd29583f9633c80b411d
Scott Horowitz <scott.horowitz@nrel.gov>
2018-11-16 06:40:07 -0700

    Squashed 'measures/HPXMLtoOpenStudio/' changes from c8aeaf492..65853992b
    65853992b Merge pull request #6 from NREL/schema_validation
    0cb41f073 Add schema validation test via nokogiri.
    b7ab9c6d9 Merge pull request #5 from NREL/latest_os_beopt
    61820dce7 Automatically apply rubocop style cop.
    0ba2cfa84 Clean up constants.
    2c27f123e Merge branch 'master' of https://github.com/NREL/OpenStudio-BEopt into utilitybills
    059e2f0e5 Test commit.
    cc510b47b Lots of file consolidation. Sync up.
    
    git-subtree-dir: measures/HPXMLtoOpenStudio
    git-subtree-split: 65853992b8aad9a0317c0ed876d1853c0064cbb6

--- tests/hpxml_translator_test.rb
+++ tests/hpxml_translator_test.rb
@@ -6,18 +6,18 @@ require 'fileutils'
 require 'json'
 
 class HPXMLTranslatorTest < MiniTest::Test
-
   def test_valid_simulations
     this_dir = File.dirname(__FILE__)
-    
+
     args_hash = {}
     args_hash['weather_dir'] = File.absolute_path(File.join(this_dir, "..", "weather"))
     args_hash['epw_output_path'] = File.absolute_path(File.join(this_dir, "in.epw"))
     args_hash['osm_output_path'] = File.absolute_path(File.join(this_dir, "in.osm"))
-    
+
     Dir["#{this_dir}/valid*.xml"].sort.each do |xml|
       puts "Testing #{xml}..."
       args_hash['hpxml_path'] = File.absolute_path(xml)
+      _test_schema_validation(this_dir, xml)
       _test_measure(args_hash)
       _test_simulation(args_hash, this_dir)
     end
@@ -26,7 +26,7 @@ class HPXMLTranslatorTest < MiniTest::Test
   def _test_measure(args_hash)
     # create an instance of the measure
     measure = HPXMLTranslator.new
-    
+
     # create an instance of a runner
     runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
 
@@ -48,28 +48,27 @@ class HPXMLTranslatorTest < MiniTest::Test
     # run the measure
     measure.run(model, runner, argument_map)
     result = runner.result
-    
+
     # show the output
     show_output(result)
 
     # assert that it ran correctly
     assert_equal("Success", result.value.valueName)
-
   end
-  
+
   def _test_simulation(args_hash, this_dir)
-  
     # Get EPW path
     hpxml_doc = REXML::Document.new(File.read(args_hash['hpxml_path']))
     weather_wmo = XMLHelper.get_value(hpxml_doc, "/HPXML/Building/BuildingDetails/ClimateandRiskZones/WeatherStation/WMO")
     epw_path = nil
-    CSV.foreach(File.join(args_hash['weather_dir'], "data.csv"), headers:true) do |row|
+    CSV.foreach(File.join(args_hash['weather_dir'], "data.csv"), headers: true) do |row|
       next if row["wmo"] != weather_wmo
+
       epw_path = File.absolute_path(File.join(args_hash['weather_dir'], row["filename"]))
       break
     end
     refute_nil(epw_path)
-        
+
     # Create osw
     osw_path = File.join(this_dir, "in.osw")
     workflow = OpenStudio::WorkflowJSON.new
@@ -84,18 +83,28 @@ class HPXMLTranslatorTest < MiniTest::Test
     steps.push(step)
     workflow.setWorkflowSteps(steps)
     workflow.saveAs(osw_path)
-    
+
     cli_path = OpenStudio.getOpenStudioCLI
     cmd = "\"#{cli_path}\" --no-ssl run -w \"#{osw_path}\""
     system(cmd)
-    
+
     # Ensure success
     out_osw = File.join(this_dir, "out.osw")
     assert(File.exists?(out_osw))
-    
+
     data_hash = JSON.parse(File.read(out_osw))
     assert_equal(data_hash["completed_status"], "Success")
-    
+  end
+
+  def _test_schema_validation(parent_dir, xml)
+    # TODO: Remove this when schema validation is included with CLI calls
+    schemas_dir = File.absolute_path(File.join(parent_dir, "..", "hpxml_schemas"))
+    hpxml_doc = REXML::Document.new(File.read(xml))
+    errors = XMLHelper.validate(hpxml_doc.to_s, File.join(schemas_dir, "HPXML.xsd"), nil)
+    if errors.size > 0
+      puts "#{xml}: #{errors.to_s}"
+    end
+    assert_equal(0, errors.size)
   end
   
 end
