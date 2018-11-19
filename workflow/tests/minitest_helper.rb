7773f9ebfc5dc9cd5362fcb1b9ebfb134f3a3b35
Scott Horowitz <scott.horowitz@nrel.gov>
2018-11-19 13:26:52 -0700

    Try #2

--- workflow/tests/minitest_helper.rb
+++ workflow/tests/minitest_helper.rb
@@ -1,14 +1,23 @@
-require 'simplecov'
-require 'codecov'
+called_from_cli = true
+begin
+  OpenStudio.getOpenStudioCLI
+rescue
+  called_from_cli = false
+end 
 
-# save to CircleCI's artifacts directory if we're on CircleCI
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
-  SimpleCov.coverage_dir(dir)
+if not called_from_cli # cli can't load codecov gem
+  require 'simplecov'
+  require 'codecov'
+
+  # save to CircleCI's artifacts directory if we're on CircleCI
+  if ENV['CIRCLE_ARTIFACTS']
+    dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+    SimpleCov.coverage_dir(dir)
+  end
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  SimpleCov.start
+  require 'minitest/autorun'
+  require 'minitest/reporters'
+
+  Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new # spec-like progress
 end
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
-SimpleCov.start
-require 'minitest/autorun'
-require 'minitest/reporters'
-
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new # spec-like progress
