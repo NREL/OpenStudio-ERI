fd5eb43da132a31ca746eec43793ceb47f79b7a7
Scott Horowitz <scott.horowitz@nrel.gov>
2018-12-18 12:25:29 -0700

    Fix mech vent output.

--- workflow/energy_rating_index.rb
+++ workflow/energy_rating_index.rb
@@ -183,7 +183,7 @@ def read_output(design, designdir, output_hpxml_path)
   design_output[:elecCeilingFan] = get_sql_query_result(sqlFile, query)
 
   # Other - Mechanical Ventilation
-  query = "SELECT Value FROM TabularDataWithStrings WHERE ReportName='EnergyMeters' AND ReportForString='Entire Facility' AND TableName='Annual and Peak Values - Electricity' AND RowName LIKE '#{Constants.ObjectNameMechanicalVentilation}%' AND ColumnName='Electricity Annual Value' AND Units='GJ'"
+  query = "SELECT SUM(Value) FROM TabularDataWithStrings WHERE ReportName='EnergyMeters' AND ReportForString='Entire Facility' AND TableName='Annual and Peak Values - Electricity' AND RowName LIKE '#{Constants.ObjectNameMechanicalVentilation}%' AND ColumnName='Electricity Annual Value' AND Units='GJ'"
   design_output[:elecMechVent] = get_sql_query_result(sqlFile, query)
 
   # Other - Recirculation pump
