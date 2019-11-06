55c47fdd9ebfd9fef4dd060f4b02300ab0b5faee
Scott Horowitz <scott.horowitz@nrel.gov>
2019-11-06 07:59:48 -0700

    Bump to v0.5.0, update sample results.

--- workflow/energy_rating_index.rb
+++ workflow/energy_rating_index.rb
@@ -1431,7 +1431,7 @@ if OpenStudio.openStudioVersion != os_version
 end
 
 if options[:version]
-  workflow_version = "0.4.0"
+  workflow_version = "0.5.0"
   puts "OpenStudio-ERI v#{workflow_version}"
   puts "OpenStudio v#{OpenStudio.openStudioLongVersion}"
   puts "EnergyPlus v#{OpenStudio.energyPlusVersion}.#{OpenStudio.energyPlusBuildSHA}"
