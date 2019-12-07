a72eb102071dbab249021f5430db37b3e20d1d35
Scott Horowitz <scott.horowitz@nrel.gov>
2019-12-07 11:04:50 -0700

    Exclude LCC objects, add FT errors/warnings.

--- workflow/tests/energy_rating_index_test.rb
+++ workflow/tests/energy_rating_index_test.rb
@@ -837,6 +837,7 @@ class EnergyRatingIndexTest < Minitest::Test
 
     # Write model to IDF
     forward_translator = OpenStudio::EnergyPlus::ForwardTranslator.new
+    forward_translator.setExcludeLCCObjects(true)
     model_idf = forward_translator.translateModel(model)
     File.open(File.join(rundir, "in.idf"), 'w') { |f| f << model_idf.to_s }
 
