361a8e856f3fa6342002978f2705edf3ccbec22f
Scott Horowitz <scott.horowitz@nrel.gov>
2020-09-24 11:55:10 -0600

    Sync. [ci skip]

--- rulesets/301EnergyRatingIndexRuleset/tests/test_validation.rb
+++ rulesets/301EnergyRatingIndexRuleset/tests/test_validation.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../../../workflow/tests/minitest_helper'
-require 'openstudio'
 require 'openstudio/ruleset/ShowRunnerOutput'
 require 'minitest/autorun'
 require 'fileutils'
--- workflow/tests/energy_rating_index_test.rb
+++ workflow/tests/energy_rating_index_test.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'minitest_helper'
-require 'openstudio'
 require 'openstudio/ruleset/ShowRunnerOutput'
 require 'minitest/autorun'
 require 'fileutils'
