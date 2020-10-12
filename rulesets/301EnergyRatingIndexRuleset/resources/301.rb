a181af20338080188ac8260252083c61e6c4069a
Scott Horowitz <scott.horowitz@nrel.gov>
2020-10-12 10:23:10 -0600

    Remove need for 999 values for new appliance inputs when using older ERI versions.

--- rulesets/301EnergyRatingIndexRuleset/resources/301.rb
+++ rulesets/301EnergyRatingIndexRuleset/resources/301.rb
@@ -1824,11 +1824,6 @@ class EnergyRatingIndex301Ruleset
       return
     end
 
-    if Constants.ERIVersions.index(@eri_version) < Constants.ERIVersions.index('2019A')
-      # This fields are required but not used
-      clothes_washer.label_usage = 999
-    end
-
     new_hpxml.clothes_washers.add(id: clothes_washer.id,
                                   is_shared_appliance: clothes_washer.is_shared_appliance,
                                   water_heating_system_idref: clothes_washer.water_heating_system_idref,
@@ -1936,14 +1931,6 @@ class EnergyRatingIndex301Ruleset
     end
 
     dishwasher = orig_hpxml.dishwashers[0]
-
-    if Constants.ERIVersions.index(@eri_version) < Constants.ERIVersions.index('2019A')
-      # These fields are required but not used
-      dishwasher.label_electric_rate = 999
-      dishwasher.label_gas_rate = 999
-      dishwasher.label_annual_gas_cost = 999
-      dishwasher.label_usage = 999
-    end
 
     new_hpxml.dishwashers.add(id: dishwasher.id,
                               is_shared_appliance: dishwasher.is_shared_appliance,
