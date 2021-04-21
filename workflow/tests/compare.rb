a53427260a8e36c211f27573350132b96524b9db
Joe Robertson <joseph.robertson@nrel.gov>
2021-04-20 17:56:11 -0700

    Skip first column.

--- workflow/tests/compare.rb
+++ workflow/tests/compare.rb
@@ -65,7 +65,7 @@ files.each do |file|
   hpxmls.sort.each do |hpxml|
     row = [hpxml]
     cols.each do |col|
-      next if col.include?('XML')
+      next if i == 0
 
       begin
         base_field = base[hpxml][col]
