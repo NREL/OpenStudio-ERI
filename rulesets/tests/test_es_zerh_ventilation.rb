5f79920dcc6f8d3ce057f6621c61ef2427bfb98e
bpark1327 <benjamin.park@nrel.gov>
2022-10-26 12:45:45 -0600

    Update tests for the ventilation system

--- rulesets/tests/test_es_zerh_ventilation.rb
+++ rulesets/tests/test_es_zerh_ventilation.rb
@@ -42,7 +42,7 @@ class EnergyStarZeroEnergyReadyHomeVentTest < MiniTest::Test
       if ['1A', '1B', '1C', '2A', '2B', '2C', '3A', '3B', '3C', '4A', '4B'].include? iecc_zone
         return HPXML::MechVentTypeSupply
       elsif ['4C', '5A', '5B', '5C', '6A', '6B', '6C', '7', '8'].include? iecc_zone
-        return HPXML::MechVentTypeBalanced
+        return HPXML::MechVentTypeHRV
       end
     else
       if ['1A', '1B', '1C', '2A', '2B', '2C', '3A', '3B', '3C', '4A', '4B'].include? iecc_zone
