3615e00335d19befcb113a05bb5caced9039bd96
Scott Horowitz <scott.horowitz@nrel.gov>
2024-10-22 19:18:04 -0600

    Update test.

--- rulesets/tests/test_ventilation.rb
+++ rulesets/tests/test_ventilation.rb
@@ -726,7 +726,8 @@ class ERIMechVentTest < Minitest::Test
     calc_type = Constants::CalcTypeERIRatedHome
     _hpxml, hpxml_bldg = _test_ruleset(hpxml_name, calc_type)
     _check_mech_vent(hpxml_bldg, [{ fantype: HPXML::MechVentTypeCFIS, flowrate: 330.0, hours: 8,
-                                    cfis_mode: HPXML::CFISModeSupplementalFan, cfis_suppl_flowrate: 110.0, cfis_suppl_power: 38.5 }])
+                                    cfis_mode: HPXML::CFISModeSupplementalFan, cfis_suppl_flowrate: 110.0, cfis_suppl_power: 38.5,
+                                    cfis_suppl_fan_sync: false }])
   end
 
   def test_mech_vent_cfm50_infiltration
