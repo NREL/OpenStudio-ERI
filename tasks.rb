2749bc6c5198a89a237bd4bf0702a05f058e818c
Scott Horowitz <scott.horowitz@nrel.gov>
2024-11-26 14:33:54 -0700

    Bugfix.

--- tasks.rb
+++ tasks.rb
@@ -2475,25 +2475,28 @@ def create_sample_hpxmls
         fail hpxml_path
       end
     end
-    zip_map = { 'USA_CO_Denver.Intl.AP.725650_TMY3.epw' => '80206',
+    zip_map = { 'USA_CO_Denver.Intl.AP.725650_TMY3.epw' => '80019',
                 'USA_OR_Portland.Intl.AP.726980_TMY3.epw' => '97214',
                 'US_CO_Boulder_AMY_2012.epw' => '80305-3447',
                 'USA_MD_Baltimore-Washington.Intl.AP.724060_TMY3.epw' => '21221',
-                'USA_TX_Dallas-Fort.Worth.Intl.AP.722590_TMY3.epw' => '75229',
+                'USA_TX_Dallas-Fort.Worth.Intl.AP.722590_TMY3.epw' => '75014',
                 'USA_MN_Duluth.Intl.AP.727450_TMY3.epw' => '55807',
                 'USA_MT_Helena.Rgnl.AP.727720_TMY3.epw' => '59602',
                 'USA_HI_Honolulu.Intl.AP.911820_TMY3.epw' => '96817',
                 'USA_FL_Miami.Intl.AP.722020_TMY3.epw' => '33134',
-                'USA_AZ_Phoenix-Sky.Harbor.Intl.AP.722780_TMY3.epw' => '85023',
+                'USA_AZ_Phoenix-Sky.Harbor.Intl.AP.722780_TMY3.epw' => '85001',
                 'ZAF_Cape.Town.688160_IWEC.epw' => '00000' }
     hpxml_bldg.zip_code = zip_map[hpxml_bldg.climate_and_risk_zones.weather_station_epw_filepath]
     if hpxml_bldg.zip_code.nil?
       fail "#{hpxml_path}: EPW location (#{hpxml_bldg.climate_and_risk_zones.weather_station_epw_filepath}) not handled. Need to update zip_map."
     end
 
-    hpxml_bldg.climate_and_risk_zones.weather_station_id = nil
-    hpxml_bldg.climate_and_risk_zones.weather_station_name = nil
-    hpxml_bldg.climate_and_risk_zones.weather_station_epw_filepath = nil
+    if hpxml_bldg.climate_and_risk_zones.weather_station_epw_filepath.include? 'TMY3'
+      # Test zipcode -> TMY3 lookup
+      hpxml_bldg.climate_and_risk_zones.weather_station_id = nil
+      hpxml_bldg.climate_and_risk_zones.weather_station_name = nil
+      hpxml_bldg.climate_and_risk_zones.weather_station_epw_filepath = nil
+    end
 
     if hpxml_path.include? 'base-location-capetown-zaf'
       if hpxml_bldg.state_code.nil?
