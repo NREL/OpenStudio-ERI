564e37cbb8ebeb970347312b807ae47a4b5d6688
Scott Horowitz <scott.horowitz@nrel.gov>
2025-07-25 14:54:14 -0600

    Squashed 'hpxml-measures/' changes from 2a8c2c87d8..d3cb26cf4c
    d3cb26cf4c Various updates.
    2c3e395b13 Fix test.
    5adba38d46 Latest results.
    b1f166af8f Create a separate sample file for water heater setpoint temperature.
    8efef88bba Merge branch 'update_sample_files3' of https://github.com/NREL/OpenStudio-HPXML into update_sample_files3
    5a5f4cb484 Update temperature capacitance multiplier so it's not the same as the default.
    9c82d113e2 Latest results.
    bb9d99c648 Update test.
    d3a163a42e Merge branch 'update_sample_files3' of https://github.com/NREL/OpenStudio-HPXML into update_sample_files3
    baf9f685f6 Update EVs.
    697df20de0 Latest results.
    9994d9566b Update test.
    2ab64bf13a Update rim joists, doors, and slab carpet.
    d8d61d5a5d Latest results.
    072a7498ea Update dehumidifiers and solar thermal.
    0fc8e862db Latest results.
    4b538d1e8a Merge branch 'update_sample_files3' of https://github.com/NREL/OpenStudio-HPXML into update_sample_files3
    1020143532 Update cooking range/oven and freezer.
    c7d7441fc3 Latest results.
    d05041cc8b Update dishwashers and refrigerators.
    73f8b423a9 Latest results.
    c72dabfed9 Update clothes washers/dryers.
    cfc46e7375 Latest results.
    df85b68787 Merge branch 'master' of https://github.com/NREL/OpenStudio-HPXML into update_sample_files3
    43e29c0b69 Merge branch 'update_sample_files3' of https://github.com/NREL/OpenStudio-HPXML into update_sample_files3
    b7934aadfa Update water heaters, switch base files to UEF.
    0e87737b48 Merge pull request #2043 from NREL/fix-ghp-loop-fluid-type
    7791f75d0f Add changelog, minor cleanup. [ci skip]
    e157fd3bcb Add reference to the OpenStudio issue in the FIXME. [ci skip]
    c724db4835 Latest results.
    10d40e4a23 Latest results.
    eb7c5d3a0e Update natvent.
    fb32523773 Latest results.
    1e670775a6 Update hvac.rb to (re)set condenser loop fluid type after supply component is added.
    7499f4dec3 Update windows/skylights.
    aa437b6e8f Latest results.
    c4f79abd9a Merge branch 'master' of https://github.com/NREL/OpenStudio-HPXML into update_sample_files3
    1c5744e620 Update walls.
    e0d921f10f Merge pull request #2042 from NREL/ef_ua_with_flow
    94fda2ee23 Minor stuff [ci skip]
    23904b04a0 Latest results.
    6ae1bda29f Merge branch 'update_sample_files3' of https://github.com/NREL/OpenStudio-HPXML into update_sample_files3
    2ea022ecd0 Update ceiling/roof R-values.
    4b108811ad Fix test_water_heater.rb for new UA calculations
    37c7fccc83 Latest results.
    617659711a Bugfix.
    6b6b1dac7b Update floor assembly R-values.
    43a5949168 Latest results.
    f47e663a9e Update a few more files.
    4a58052fab Latest results.
    a99efad4c3 Update comment in code on how we calculate UA with EF
    f6f80b9e63 Update Changelog.md
    9a5fcf53a8 Merge branch 'master' of https://github.com/NREL/OpenStudio-HPXML into ef_ua_with_flow
    a44325298e remove debug print statements
    ac693061c4 For testing: calculate UA based on EF with and without f_low
    
    git-subtree-dir: hpxml-measures
    git-subtree-split: d3cb26cf4c853284c6e21cf629d1432f4fdf723f

--- HPXMLtoOpenStudio/tests/test_enclosure.rb
+++ HPXMLtoOpenStudio/tests/test_enclosure.rb
@@ -590,7 +590,7 @@ class HPXMLtoOpenStudioEnclosureTest < Minitest::Test
       os_window = model.getSubSurfaces.find { |w| w.name.to_s == window.id }
       os_simple_glazing = os_window.construction.get.to_LayeredConstruction.get.getLayer(0).to_SimpleGlazing.get
 
-      assert_equal(0.36, os_simple_glazing.solarHeatGainCoefficient)
+      assert_equal(0.352, os_simple_glazing.solarHeatGainCoefficient)
       assert_in_epsilon(0.2936, UnitConversions.convert(os_simple_glazing.uFactor, 'W/(m^2*K)', 'Btu/(hr*ft^2*F)'), 0.001)
     end
 
@@ -896,7 +896,6 @@ class HPXMLtoOpenStudioEnclosureTest < Minitest::Test
                       'base-foundation-conditioned-crawlspace.xml' => 68.0, # foundation adjacent to conditioned space, IECC zone 5
                       'base-foundation-slab.xml' => 68.0, # foundation adjacent to conditioned space, IECC zone 5
                       'base-foundation-unconditioned-basement.xml' => 41.4, # foundation adjacent to unconditioned basement w/ ceiling insulation
-                      'base-foundation-unconditioned-basement-wall-insulation.xml' => 56.0, # foundation adjacent to unconditioned basement w/ wall insulation
                       'base-foundation-unvented-crawlspace.xml' => 38.6, # foundation adjacent to unvented crawlspace w/ ceiling insulation
                       'base-foundation-vented-crawlspace.xml' => 36.9, # foundation adjacent to vented crawlspace w/ ceiling insulation
                       'base-location-miami-fl.xml' => 78.0 } # foundation adjacent to conditioned space, IECC zone 1
