2df24bc04e10c303584b87d8c81be27fdc3eaf0c
bpark1327 <benjamin.park@nrel.gov>
2026-06-22 15:22:33 -0600

    Add a dehumidifier to Ref Home

--- rulesets/resources/301ruleset.rb
+++ rulesets/resources/301ruleset.rb
@@ -2270,6 +2270,15 @@ module ERI_301_Ruleset
   def self.set_appliances_dehumidifier_reference(orig_bldg, new_bldg)
     return if Constants::ERIVersions.index(@eri_version) < Constants::ERIVersions.index('2019AB')
 
+    if orig_bldg.dehumidifiers.empty?
+      new_bldg.dehumidifiers.add(id: "Dehumidifier",
+                                 type: HPXML::DehumidifierTypePortable,
+                                 rh_setpoint: 0.60,  # TODO: 60% vs 55%
+                                 fraction_served: 1.0,
+                                 location: HPXML::LocationConditionedSpace)
+      return
+    end
+
     orig_bldg.dehumidifiers.each do |dehumidifier|
       reference_values = Defaults.get_dehumidifier_values(dehumidifier.capacity)
       new_bldg.dehumidifiers.add(id: dehumidifier.id,
